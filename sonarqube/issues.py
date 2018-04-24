eb5cd9e6ef0682fc35430fad871cc35d95409523
Olivier Korach <olivier.korach@gmail.com>
2018-04-24 06:10:16 -0300

    Organized in modules

--- sonarqube/issues.py
+++ sonarqube/issues.py
@@ -13,6 +13,22 @@ class UnknownIssueError(ApiError):
     pass
 
 
+class IssueComments:
+    def __init__(self, json):
+        self.json = json
+
+    def sort(self):
+        sorted_comment = dict()
+        for comment in self.json:
+            sorted_comment[comment['createdAt']] = ('comment', comment)
+        return sorted_comment
+
+    def size(self):
+        n = 0
+        for items in self.json:
+            n = n + 1
+        return n
+
 class IssueChangeLog:
     def __init__(self, issue_key):
         params = dict(format='json', issue=issue_key)
@@ -20,8 +36,27 @@ class IssueChangeLog:
         data = json.loads(resp.text)
         self.json = data['changelog']
 
-class Issue:
+    def sort(self):
+        sorted_log = dict()
+        for log in self.json:
+            sorted_log[log['creationDate']] = ('log', log)
+        return sorted_log
 
+    def size(self):
+        n = 0
+        for items in self.json:
+            n = n + 1
+        return n
+
+    def to_string(self):
+        """Dumps the object in a string"""
+        return json.dumps(self.json, sort_keys=True, indent=3, separators=(',', ': '))
+
+    def get_json(self):
+        return self.json
+
+
+class Issue:
     def __init__(self, issue_key):
         self.id = issue_key
         self.json = ''
@@ -34,13 +69,12 @@ class Issue:
         self.rule = ''
         self.project = ''
         self.language = ''
-        self.changelog = ''
+        self.changelog = None
         self.comments = ''
-        self.line = ''
+        self.line = None
         self.component = ''
-
-    def __init__(self, json):
-        self.feed(json)
+        self.message = ''
+        self.debt = ''
 
     def feed(self, json):
         self.id = json['key']
@@ -50,25 +84,42 @@ class Issue:
         self.author = json['author']
         self.assignee = '' # json['assignee']
         self.status = json['status']
-        self.resolution = json['resolution']
+        try:
+            self.line = json['line']
+        except KeyError:
+            self.line = None
+
+        self.resolution = '' # json['resolution']
         self.rule = json['rule']
         self.project = json['project']
         self.language = ''
-        self.changelog = '' # To read with API
+        self.changelog = None
         self.comments = json['comments']
         self.component = json['component']
-        self.line = json['line']
+        try:
+            self.hash = json['hash']
+        except KeyError:
+            self.hash = None
+        try:
+            self.message = json['message']
+        except KeyError:
+            self.message = None
+        self.debt = json['debt']
 
     def read(self):
-        resp = requests.get(url=sonarqube.env.get_url() + '/api/issues/search', auth=sonarqube.env.get_credentials(), params=dict(issues=self.id))
+        resp = requests.get(url=sonarqube.env.get_url() + '/api/issues/search', auth=sonarqube.env.get_credentials(), params=dict(issues=self.id, additionalFields='_all'))
         self.feed(resp.issues[0])
 
-    def get_changelog(self):
-        self.changelog = IssueChangeLog(self.id)
+    def get_changelog(self, force_api = False):
+        if (force_api or self.changelog is None):
+            self.changelog = IssueChangeLog(self.id)
+            print('---In get_changelog----')
+            print(self.changelog.to_string())
+        return self.changelog
         
     def add_comment(self, comment_text):
         params = dict(issue=self.id, text=comment_text)
-        resp = requests.post(url=sonarqube.env.get_url() + '/api/issues/add_comment', auth=sonarqube.env.get_credentials(), params=params)
+        requests.post(url=sonarqube.env.get_url() + '/api/issues/add_comment', auth=sonarqube.env.get_credentials(), params=params)
 
     # def delete_comment(self, comment_id):
 
@@ -103,68 +154,81 @@ class Issue:
     def get_status(self):
         return self.status
     
-    def tostring(self):
+    def get_comments(self):
+        try:
+            self.comments = IssueComments(self.json['comments'])
+        except KeyError:
+            self.comments = None
+        return self.comments
+
+    def to_string(self):
         """Dumps the object in a string"""
         return json.dumps(self.json, sort_keys=True, indent=3, separators=(',', ': '))
 
+    def print_issue(self):
+        """Prints the issue"""
+        print(self.to_string())
 
-def has_been_marked_as_statuses(diffs, statuses):
-    for diff in diffs:
-        if diff["key"] == "resolution":
-            for status in statuses:
-                if diff["newValue"] == status:
+    def has_been_marked_as_wont_fix(self):
+        changelog = self.get_changelog()
+        for log in changelog:
+            for diff in log['diffs']:
+                if diff["key"] == "resolution" and diff["newValue"] == "WONTFIX":
                     return True
-    return False
+        return False
 
+    def has_been_marked_as_false_positive(self):
+        changelog = self.get_changelog()
+        for log in changelog:
+            for diff in log['diffs']:
+                if diff["key"] == "resolution" and diff["newValue"] == "FALSE-POSITIVE":
+                    return True
+        return False
 
-def has_been_marked_as_false_positive(issue_key):
-    changelog = get_changelog(issue_key)
-    for log in changelog:
-        for diff in log['diffs']:
-            if diff["key"] == "resolution" and diff["newValue"] == "FALSE-POSITIVE":
+    def has_been_marked_as_statuses(self, diffs, statuses):
+        for diff in diffs:
+            if diff["key"] == "resolution":
+                for status in statuses:
+                    if diff["newValue"] == status:
+                        return True
+        return False
+
+    def print_change_log(self):
+        events_by_date = self.get_changelog().sort()
+        comments_by_date = self.get_comments().sort()
+        for date in comments_by_date:
+            events_by_date[date] = comments_by_date[date]
+        for date in sorted(events_by_date):
+            print(date, ':')
+            # print_object(events_by_date[date])
+    
+    def get_key(self):
+        return self.id
+
+            
+    def identical_to(self, another_issue):
+        return (self.rule == another_issue.rule and self.component == another_issue.component and
+            self.message == another_issue.message and self.debt == another_issue.debt and self.hash == another_issue.hash)
+
+    def was_fp_or_wf(self):
+        changelog = self.get_changelog()
+        print('------- ISS ChangeLog--------')
+        print(changelog)
+        print(changelog.to_string())
+        for log in changelog.get_json():
+            if is_log_a_closed_fp(log) or is_log_a_closed_wf(log) or is_log_a_severity_change(log) or is_log_a_type_change(
+                    log):
                 return True
-    return False
+        return False
 
-
-def has_been_marked_as_wont_fix(issue_key):
-    changelog = get_changelog(issue_key)
-    for log in changelog:
-        for diff in log['diffs']:
-            if diff["key"] == "resolution" and diff["newValue"] == "WONTFIX":
-                return True
-    return False
-
-
+#------------------------------- Static methods --------------------------------------
 def check_fp_transition(diffs):
     print("----------------- DIFFS     -----------------")
-    print_object(diffs)
+    #print_object(diffs)
     if diffs[0]['key'] == "resolution" and (
         diffs[1]["oldValue"] == "FALSE-POSITIVE" or diffs[1]["oldValue"] == "WONTFIX") and diffs[0]["newValue"] == "FIXED":
         return True
     return False
-
-
-def print_object(o):
-    print(json.dumps(o, indent=3, sort_keys=True))
-
-
-
-
-
-def get_comments(issue_key):
-    # print('Searching comments for issue key ', issue_key)
-    params = dict(format='json', issues=issue_key, additionalFields='comments')
-    resp = requests.get(url=sonarqube.env.get_url() + 'api/issues/search', auth=sonarqube.env.get_credentials(), params=params)
-    data = json.loads(resp.text)
-    return data['issues'][0]['comments']
-
-
-def sort_changelog(changelog):
-    sorted_log = dict()
-    for log in changelog:
-        sorted_log[log['creationDate']] = ('log', log)
-    return sorted_log
-
 
 def sort_comments(comments):
     sorted_comments = dict()
@@ -173,18 +237,224 @@ def sort_comments(comments):
     return sorted_comments
 
 def search(project_key, sq_url = sonarqube.env.get_url()):
-    params = dict(ps='1', componentKeys=project_key, additionalFields='_all')
+    params = dict(ps='500', componentKeys=project_key, additionalFields='_all')
     resp = requests.get(url=sq_url + '/api/issues/search', auth=sonarqube.env.get_credentials(), params=params)
     data = json.loads(resp.text)
     json.dump(data, sys.stdout, sort_keys=True, indent=3, separators=(',', ': '))
     print("Number of issues:", data['paging']['total'])
     all_issues = []
     for json_issue in data['issues']:
-        all_issues = all_issues + [Issue(json_issue)]
- 
-    #all_json_issues = data['issues']
-    #json.dump(all_json_issues, sys.stdout, sort_keys=True, indent=3, separators=(',', ': '))
-    #for issue in all_issues:
-    #    print(issue.toString)
+        issue = Issue(0)
+        issue.feed(json_issue)
+        all_issues = all_issues + [issue]
+        print('----issues.ISSUE-------------------------------------------------------------')
+        json.dump(json_issue, sys.stdout, sort_keys=True, indent=3, separators=(',', ': '))
+        #print(issue.toString)
     return all_issues
 
+
+
+
+def apply_changelog(new_issue, closed_issue, do_it_really=True):
+    events_by_date = closed_issue.get_changelog().sort()
+    comments_by_date = closed_issue.get_comments().sort()
+    key = new_issue.id
+    for date in comments_by_date:
+        events_by_date[date] = comments_by_date[date]
+    if do_it_really:
+        print('   Not joking I am doing it')
+    for date in sorted(events_by_date):
+        # print_object(events_by_date[date])
+        is_applicable_event = True
+
+        if events_by_date[date][0] == 'log' and is_log_a_severity_change(events_by_date[date][1]):
+            params = dict(issue=key, severity=get_log_new_severity(events_by_date[date][1]))
+            operation = 'Changing severity to: ' + params['severity']
+            api = 'issues/set_severity'
+        elif events_by_date[date][0] == 'log' and is_log_a_type_change(events_by_date[date][1]):
+            params = dict(issue=key, type=get_log_new_type(events_by_date[date][1]))
+            operation = 'Changing type to: ' + params['type']
+            api = 'issues/set_type'
+        elif events_by_date[date][0] == 'log' and is_log_a_reopen(events_by_date[date][1]):
+            params = dict(issue=key, type='reopen')
+            operation = 'Reopening issue'
+            api = 'issues/do_transition'
+        elif events_by_date[date][0] == 'log' and is_log_a_resolve_as_fp(events_by_date[date][1]):
+            params = dict(issue=key, transition='falsepositive')
+            operation = 'Setting as False Positive'
+            api = 'issues/do_transition'
+        elif events_by_date[date][0] == 'log' and is_log_a_resolve_as_wf(events_by_date[date][1]):
+            params = dict(issue=key, transition='wontfix')
+            operation = 'Setting as wontfix'
+            api = 'issues/do_transition'
+        elif events_by_date[date][0] == 'log' and is_log_an_assignee(events_by_date[date][1]):
+            params = dict(issue=key, assignee=get_log_assignee(events_by_date[date][1]))
+            operation = 'Assigning issue to: ' + params['assignee']
+            api = 'issues/assign'
+        elif events_by_date[date][0] == 'log' and is_log_a_tag_change(events_by_date[date][1]):
+            params = dict(key=key, tags=get_log_new_tag(events_by_date[date][1]).replace(' ', ','))
+            operation = 'Setting new tags to: ' + params['tags']
+            api = 'issues/set_tags'
+        elif events_by_date[date][0] == 'comment' and is_log_a_comment(events_by_date[date][1]):
+            params = dict(issue=key, text=events_by_date[date][1]['markdown'])
+            operation = 'Adding comment: ' + params['text']
+            api = 'issues/add_comment'
+        else:
+            is_applicable_event = False
+            api = ''
+
+
+        if is_applicable_event:
+            if do_it_really:
+                print('   ' + operation)
+                resp = requests.post(url=sonarqube.env.get_url() + '/api/' + api, auth=sonarqube.env.get_credentials(), params=params)
+                if resp.status_code != 200:
+                    print('HTTP Error ' + str(resp.status_code) + ' from SonarQube API query')
+            else:
+                print('   DRY RUN for ' + operation)
+
+
+
+
+def get_log_date(log):
+    return log['creationDate']
+
+
+def is_log_a_closed_resolved_as(log, old_value):
+    cond1 = False
+    cond2 = False
+
+    for diff in log['diffs']:
+        if diff['key'] == 'resolution' and 'newValue' in diff and diff['newValue'] == 'FIXED' and 'oldValue' in diff and \
+                        diff['oldValue'] == old_value:
+            cond1 = True
+        if diff['key'] == 'status' and 'newValue' in diff and diff['newValue'] == 'CLOSED' and 'oldValue' in diff and \
+                        diff['oldValue'] == 'RESOLVED':
+            cond2 = True
+    return cond1 and cond2
+
+
+def is_log_a_closed_wf(log):
+    return is_log_a_closed_resolved_as(log, 'WONTFIX')
+
+
+def is_log_a_comment(log):
+    return True
+
+
+def is_log_an_assign(log):
+    return False
+
+
+def is_log_a_tag(log):
+    return False
+
+def is_log_a_closed_fp(log):
+    return is_log_a_closed_resolved_as(log, 'FALSE-POSITIVE')
+
+def is_log_a_resolve_as(log, resolve_reason):
+    cond1 = False
+    cond2 = False
+    for diff in log['diffs']:
+        if diff['key'] == 'resolution' and 'newValue' in diff and diff['newValue'] == resolve_reason:
+            cond1 = True
+        if diff['key'] == 'status' and 'newValue' in diff and diff['newValue'] == 'RESOLVED':
+            cond2 = True
+    return cond1 and cond2
+
+def is_log_an_assignee(log):
+    for diff in log['diffs']:
+        if diff['key'] == 'assignee':
+            return True
+
+def is_log_a_reopen(log):
+    cond1 = False
+    cond2 = False
+    for diff in log['diffs']:
+        if diff['key'] == 'resolution':
+            cond1 = True
+        if diff['key'] == 'status' and 'newValue' in diff and diff['newValue'] == 'REOPENED':
+            cond2 = True
+    return cond1 and cond2
+
+def is_log_a_resolve_as_fp(log):
+    return is_log_a_resolve_as(log, 'FALSE-POSITIVE')
+
+def is_log_a_resolve_as_wf(log):
+    return is_log_a_resolve_as(log, 'WONTFIX')
+
+
+def log_change_type(log):
+    return log['diffs'][0]['key']
+
+
+def is_log_a_severity_change(log):
+    return log_change_type(log) == 'severity'
+
+
+def is_log_a_type_change(log):
+    return log_change_type(log) == 'type'
+
+
+def is_log_an_assignee_change(log):
+    return log_change_type(log) == 'assignee'
+
+
+def is_log_a_tag_change(log):
+    return log_change_type(log) == 'tags'
+
+
+def get_log_new_value(log, key_type):
+    for diff in log['diffs']:
+        if diff['key'] == key_type:
+            return diff['newValue']
+    return 'undefined'
+
+
+def get_log_assignee(log):
+    return get_log_new_value(log, 'assignee')
+
+
+def get_log_new_severity(log):
+    return get_log_new_value(log, 'severity')
+
+
+def get_log_new_type(log):
+    return get_log_new_value(log, 'type')
+
+
+def get_log_new_tag(log):
+    return get_log_new_value(log, 'tags')
+
+
+def identical_attributes(o1, o2, key_list):
+    for key in key_list:
+        if o1[key] != o2[key]:
+            return False
+    return True
+
+
+def search_siblings(closed_issue, issue_list, only_new_issues=True):
+    siblings = []
+    for issue in issue_list:
+        #if identical_attributes(closed_issue, iss, ['rule', 'component', 'message', 'debt']):
+        print("Looking at CLOSED " + closed_issue.to_string())
+        print("Looking at ISSUE " + issue.to_string())
+        if issue.identical_to(closed_issue):
+            if only_new_issues:
+                if issue.get_changelog.size() == 0:
+                    # Add issue only if it has no change log, meaning it's brand new
+                    print("Appending issue")
+                    siblings.append(issue)
+            else:
+                siblings.append(issue)
+    return siblings
+
+
+
+
+
+def print_issue(issue):
+    for attr in ['rule', 'component', 'message', 'debt', 'author', 'key', 'status']:
+        print (issue[attr], ',')
+    print()
--- sonarqube/measures.py
+++ sonarqube/measures.py
@@ -2,7 +2,7 @@
 
 import json
 import requests
-import env
+import sonarqube.env
 
 class Measure:
 
@@ -12,7 +12,7 @@ class Measure:
 
     def read(self, project_key, metric_key):
         params = dict(component=project_key, metricKeys=metric_key)
-        resp = requests.get(url=root_url + '/api/measures/component', auth=credentials, params=params)
+        resp = requests.get(url=sonarqube.env.get_url() + '/api/measures/component', auth=sonarqube.env.get_credentials(), params=params)
         data = json.loads(resp.text)
         return data['component']['measures']
 
@@ -20,8 +20,7 @@ class Measure:
 
 def load_measures(project_key, metrics_list):
     params = dict(component=project_key, metricKeys=metrics_list)
-    url = env.get_url()
-    resp = requests.get(url=url + '/api/measures/component', auth=env.get_credentials(), params=params)
+    resp = requests.get(url=sonarqube.env.get_url() + '/api/measures/component', auth=sonarqube.env.get_credentials(), params=params)
     data = json.loads(resp.text)
     return data['component']['measures']
 
