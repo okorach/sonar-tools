ada4f0635a14e8fabae47f275fc1ff0d6931f5b4
Olivier Korach <olivier.korach@gmail.com>
2019-01-09 19:33:16 +0000

    Get project name for extracts

--- sonarqube/projects.py
+++ sonarqube/projects.py
@@ -2,25 +2,32 @@
 
 import json
 import requests
-import env
+from sonarqube.env import get_url, get_credentials, json_dump_debug
+import sys
 
 class Project:
 
     def __init__(self):
         self.name = ''
         self.key = ''
+    
+    def get_name(self):
+        params = dict(projets=self.key)
+        resp = requests.get(url=get_url() + '/api/projects/search', auth=get_credentials(), params=params)
+        data = json.loads(resp.text)
+        return(data['components']['name'])
 
 def count(include_applications):
     qualifiers = "TRK,APP" if include_applications else "TRK"
     params = dict(ps=3, qualifiers=qualifiers)
-    resp = requests.get(url=env.get_url() + '/api/projects/search', auth=env.get_credentials(), params=params)
+    resp = requests.get(url=get_url() + '/api/projects/search', auth=get_credentials(), params=params)
     data = json.loads(resp.text)
     return(data['paging']['total'])
 
 def get_projects(include_applications, page_size=500, page_nbr=1):
     qualifiers = "TRK,APP" if include_applications else "TRK"
     params = dict(ps=page_size, p=page_nbr, qualifiers=qualifiers)
-    resp = requests.get(url=env.get_url() + '/api/projects/search', auth=env.get_credentials(), params=params)
+    resp = requests.get(url=get_url() + '/api/projects/search', auth=get_credentials(), params=params)
     data = json.loads(resp.text)
     return data['components']
 
@@ -30,3 +37,10 @@ def get_projects_list():
     for prj in projects:
         prjlist.append(prj['key'])
     return prjlist
+
+def get_project_name(key):
+    params = dict(projects=key)
+    resp = requests.get(url=get_url() + '/api/projects/search', auth=get_credentials(), params=params)
+    data = json.loads(resp.text)
+    json_dump_debug(data)
+    return data['components'][0]['name']
