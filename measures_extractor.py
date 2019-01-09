53e3edfa9af5cc1af8b5ada27d80e38b2ce494e2
Olivier Korach <olivier.korach@gmail.com>
2019-01-09 19:22:13 +0000

    ease selection of metrics and output main metrics first

--- measures_extractor.py
+++ measures_extractor.py
@@ -1,55 +1,76 @@
-#!/usr/local/bin/python
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+import re
 import json
 import requests
-import string
-import sonarqube.measures
-import sonarqube.projects
-import sonarqube.env
 import argparse
-
-def parse_args():
-    global __project_key
-    parser = argparse.ArgumentParser(
-            description='Extract measures of projects')
-    parser.add_argument('-p', '--projectKey', help='Project key of the project to search', required=False)
-    parser.add_argument('-t', '--token',
-                        help='Token to authenticate to SonarQube - Unauthenticated usage is not possible',
-                        required=True)
-    parser.add_argument('-u', '--url', help='Root URL of the SonarQube server, default is http://localhost:9000',
-                        required=False)
-
-    args = parser.parse_args()
-
-    __project_key = args.projectKey
-    sonarqube.env.set_token(args.token)
-    sonarqube.env.set_url(args.url if args.url != None else "http://localhost:9000")
+import sonarqube.measures as measures
+import sonarqube.metrics as metrics
+import sonarqube.projects as projects
+import sonarqube.env as env
 
 
-parse_args()
+MAIN_METRICS = 'bugs,reliability_rating,vulnerabilities,security_rating,code_smells,' + \
+    'sqale_rating,technical_debt,coverage,duplications,new_bugs,new_vulnerabilities,new_code_smells,' + \
+    'new_technical_debt,new_maintainability_rating,coverage,duplicated_lines_density,' + \
+    'new_coverage,new_duplicated_lines_density'
+
+def diff(first, second):
+    second = set(second)
+    return [item for item in first if item not in second]
+
+
+parser = argparse.ArgumentParser(description='Extract measures of projects')
+parser.add_argument('-t', '--token', required=True,
+                    help='Token to authenticate to SonarQube - Unauthenticated usage is not possible')
+parser.add_argument('-u', '--url', required=False, default='http://localhost:9000',
+                    help='Root URL of the SonarQube server, default is http://localhost:9000')
+parser.add_argument('-m', '--metricKeys', required=False, help='Comma separated list of metrics or _all or _main')
+
+args = parser.parse_args()
+env.set_token(args.token)
+env.set_url(args.url)
 # Mandatory script input parameters
 csv_sep = ";"
 
-metrics = 'ncloc,new_violations,new_bugs,complexity,coverage,sqale_index'
-project_list = sonarqube.projects.get_projects(True)
+if args.metricKeys == '_all':
+    metrics = metrics.get_all_metrics_csv()
+elif args.metricKeys == '_main':
+    metrics = MAIN_METRICS
+elif args.metricKeys is not None:
+    metrics = args.metricKey
+else:
+    metrics = MAIN_METRICS
 
-print("key" + csv_sep + "name" + csv_sep + "last analysis" + csv_sep + string.replace(metrics, ",", csv_sep))
-metrics_list = metrics.split(",")
+print ("Project Key%sProject Name%sLast Analysis" % (csv_sep, csv_sep), end=csv_sep)
+metrics_list = re.split(',', metrics)
+main_metrics_list = re.split(',', MAIN_METRICS)
+if args.metricKeys == '_all':
+    for m in re.split(',', MAIN_METRICS):
+        print ("%s" % m, end=csv_sep)
+    metrics_list = diff(metrics_list, main_metrics_list)
 
+for m in metrics_list:
+    print ("%s" % m, end=csv_sep)
+print('')
+
+project_list = projects.get_projects(True)
 for project in project_list:
     key = project['key']
     p_name = project['name']
     last_analysis = project['lastAnalysisDate'] if 'lastAnalysisDate' in project else 'Not analyzed yet'
-    line = key + csv_sep + p_name + csv_sep + last_analysis
-    
-    all_measures = sonarqube.measures.load_measures(key, metrics)
+    all_measures = measures.load_measures(key, metrics)
     p_meas = {}
     for measure in all_measures:
         name = measure['metric'] if 'metric' in measure else ''
         value = measure['value'] if 'value' in measure else ''
         p_meas[name] = value
-    for measure in metrics_list:
-        line = line + csv_sep + p_meas[measure] if measure in p_meas else line + csv_sep
+    line = ''
+    print("%s%s%s%s%s" % (key, csv_sep, p_name, csv_sep, last_analysis), end=csv_sep)
+    if args.metricKeys == '_all':
+        for metric in main_metrics_list:
+            line = line + csv_sep + p_meas[metric].replace(csv_sep, '|') if metric in p_meas else line + csv_sep
+    for metric in metrics_list:
+        line = line + csv_sep + p_meas[metric].replace(csv_sep, '|') if metric in p_meas else line + csv_sep
     print(line)
 
-
-print(str(sonarqube.projects.count(True)) + " PROJECTS")
+print("%d PROJECTS" % projects.count(True))
