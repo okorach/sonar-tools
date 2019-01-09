ee74863ee617da7dc6a8f096043407bf1f5fb665
Olivier Korach <olivier.korach@gmail.com>
2019-01-09 19:32:20 +0000

    Get measure history

--- sonarqube/measures.py
+++ sonarqube/measures.py
@@ -1,27 +1,31 @@
-#!python3
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
 
 import json
 import requests
-import env
+import sonarqube.env as env
+import sonarqube.sqobject
 
-class Measure:
+class Measure (sonarqube.sqobject.SqObject):
 
-    def __init__(self):
-        self.name = ''
-        self.value = ''
+    def __init__(self, name = None, value = None, **kwargs):
+        self.name = name
+        self.value = value
+        self.history = None
+        self.env = kwargs['env']
 
-    def read(self, project_key, metric_key, myenv):
+    def read(self, project_key, metric_key):
         parms = dict(component=project_key, metricKeys=metric_key)
-        if (myenv is None):
-            resp = env.get('/api/measures/component',  parms)
-        else:
-            resp = myenv.get('/api/measures/component',  parms)
+        resp = self.get('/api/measures/component',  parms)
         data = json.loads(resp.text)
         return data['component']['measures']
 
+    def get_history(self, project_key):
+        parms = dict(component=project_key, metrics=self.name, ps=1000)
+        resp = self.get('/api/measures/search_history',  parms)
+        data = json.loads(resp.text)
+        return data['component']['measures']
 
-
-def load_measures(project_key, metrics_list, myenv):
+def load_measures(project_key, metrics_list, myenv = None):
     parms = dict(component=project_key, metricKeys=metrics_list)
     if (myenv is None):
         resp = env.get('/api/measures/component',  parms)
