efbaefd0e8327871fd127d696ea308e2290be820
Olivier Korach <olivier.korach@gmail.com>
2019-08-22 10:16:53 +0200

    Fixes

--- sonarqube/projects.py
+++ sonarqube/projects.py
@@ -1,33 +1,42 @@
 #!python3
 
+import sys
 import json
 import requests
-from sonarqube.env import get_url, get_credentials, json_dump_debug
-import sys
+import sonarqube.env as env
+# from sonarqube.env import get_url, get_credentials, json_dump_debug
+
 
 class Project:
 
     def __init__(self):
         self.name = ''
         self.key = ''
-    
+
     def get_name(self):
         params = dict(projets=self.key)
         resp = requests.get(url=get_url() + '/api/projects/search', auth=get_credentials(), params=params)
         data = json.loads(resp.text)
         return(data['components']['name'])
 
-def count(include_applications):
+def count(include_applications, myenv = None):
     qualifiers = "TRK,APP" if include_applications else "TRK"
     params = dict(ps=3, qualifiers=qualifiers)
-    resp = requests.get(url=get_url() + '/api/projects/search', auth=get_credentials(), params=params)
+    if myenv is None:
+        resp = env.get('/api/projects/search', params)
+    else:
+        resp = myenv.get('/api/projects/search', params)
     data = json.loads(resp.text)
     return(data['paging']['total'])
 
-def get_projects(include_applications, page_size=500, page_nbr=1):
+def get_projects(include_applications,  myenv = None, page_size=500, page_nbr=1):
     qualifiers = "TRK,APP" if include_applications else "TRK"
     params = dict(ps=page_size, p=page_nbr, qualifiers=qualifiers)
-    resp = requests.get(url=get_url() + '/api/projects/search', auth=get_credentials(), params=params)
+    if myenv is None:
+        resp = env.get('/api/projects/search',  params)
+    else:
+        resp = myenv.get('/api/projects/search', params)
+#        resp = requests.get(url=get_url() + '/api/projects/search', auth=get_credentials(), params=params)
     data = json.loads(resp.text)
     return data['components']
 
@@ -38,9 +47,12 @@ def get_projects_list():
         prjlist.append(prj['key'])
     return prjlist
 
-def get_project_name(key):
+def get_project_name(key, myenv):
     params = dict(projects=key)
-    resp = requests.get(url=get_url() + '/api/projects/search', auth=get_credentials(), params=params)
+    if myenv is None:
+        resp = env.get('/api/projects/search', params)
+    else:
+        resp = myenv.get('/api/projects/search', params)
     data = json.loads(resp.text)
-    json_dump_debug(data)
+    #env.json_dump_debug(data)
     return data['components'][0]['name']
