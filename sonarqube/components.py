b3cfd04655639505423bd70cfc59cdf5d7d75459
Olivier Korach <olivier.korach@gmail.com>
2019-09-03 20:55:58 +0200

    bug fix subcomponent split

--- sonarqube/components.py
+++ sonarqube/components.py
@@ -17,14 +17,14 @@ class Component:
 
     def get_subcomponents(self):
         params = dict(component=self.key, strategy='children', ps=500, p=1)
-        if sqenv is None:
+        if self.sqenv is None:
             resp = env.get('/api/components/tree', params)
         else:
             resp = self.sqenv.get('/api/components/tree', params)
         data = json.loads(resp.text)
         comps = []
         for comp in data['components']:
-            comps = comps + Component(key=comp['key'], name=comp['name'], sqenv=self.sqenv)
+            comps.append(Component(key=comp['key'], name=comp['name'], sqenv=self.sqenv))
         return comps
 
     def get_number_of_filtered_issues(self, **kwargs):
@@ -39,21 +39,22 @@ class Component:
             self.nbr_issues = self.get_number_of_filtered_issues(**{'componentKey': self.key})
         return self.nbr_issues
 
-
-    def get_all_issues(self):
-        return self.get_issues()
+    def get_all_issues(self, **kwargs):
+        return self.get_issues(**kwargs)
 
     def get_oldest_issue_date(self):
         ''' Returns the oldest date of all issues found '''
         return issues.get_oldest_issue(sqenv=self.sqenv, **{'componentKeys': self.key})
 
-    def get_newest_issue(self):
+    def get_newest_issue_date(self):
         ''' Returns the newest date of all issues found '''
         return issues.get_newest_issue(sqenv=self.sqenv, **{'componentKeys': self.key})
 
     def get_issues(self, **kwargs):
         kwargs['componentKeys'] = self.key
-        oldest = self.get_oldest_issue_date(sqenv=self.sqenv, **kwargs)
+        env.debug('=' * 40)
+        env.debug(kwargs)
+        oldest = self.get_oldest_issue_date()
         if oldest is None:
             return []
 
@@ -61,28 +62,36 @@ class Component:
         issue_list = []
         if nbr_issues <= 10000:
             issue_list = issues.search_all_issues(sqenv=self.sqenv, **kwargs)
+            for i in issue_list:
+                env.debug(i.to_csv())
         elif 'createdAfter' not in kwargs or 'createdBefore' not in kwargs:
             startdate = datetime.datetime.strptime(oldest, '%Y-%m-%dT%H:%M:%S%z')
-            enddate = datetime.datetime.strptime(self.get_newest_issue(), '%Y-%m-%dT%H:%M:%S%z')
+            enddate = datetime.datetime.strptime(self.get_newest_issue_date(), '%Y-%m-%dT%H:%M:%S%z')
             kwargs['createdAfter']  = "%04d-%02d-%02d" % (startdate.year, startdate.month, startdate.day)
             kwargs['createdBefore'] = "%04d-%02d-%02d" % (enddate.year, enddate.month, enddate.day)
             issue_list = self.get_issues(**kwargs)
         elif kwargs['createdAfter'] != kwargs['createdBefore']:
             startdate = datetime.datetime.strptime(kwargs['createdAfter'], '%Y-%m-%d')
             enddate = datetime.datetime.strptime(kwargs['createdBefore'], '%Y-%m-%d')
-            mid_date = startdate + datetime.timedelta(days=abs((enddate - startdate).days) // 2)
-            kwargs['createdAfter']  = "%04d-%02d-%02d" % (mid_date.year, mid_date.month, mid_date.day)
-            kwargs['createdBefore'] = "%04d-%02d-%02d" % (enddate.year, enddate.month, enddate.day)
-            issue_list = self.get_issues(**kwargs)
+            diffdays = abs((enddate - startdate).days)
+            env.debug("diffdays = %d" % diffdays)
+            mid_date = startdate + datetime.timedelta(days=diffdays//2)
             kwargs['createdAfter']  = "%04d-%02d-%02d" % (startdate.year, startdate.month, startdate.day)
             kwargs['createdBefore'] = "%04d-%02d-%02d" % (mid_date.year, mid_date.month, mid_date.day)
+            issue_list = self.get_issues(**kwargs)
+            if diffdays <= 1:
+                mid_date = enddate
+            else:
+                mid_date = mid_date + datetime.timedelta(days=1)
+            kwargs['createdAfter']  = "%04d-%02d-%02d" % (mid_date.year, mid_date.month, mid_date.day)
+            kwargs['createdBefore'] = "%04d-%02d-%02d" % (enddate.year, enddate.month, enddate.day)
             issue_list = issue_list + self.get_issues(**kwargs)
         else:
             issue_list = []
             for comp in self.get_subcomponents():
-                issue_list = issue_list + comp.get_all_issues()
+                issue_list = issue_list + comp.get_all_issues(**kwargs)
 
-        env.debug("For component %s, %d issues found after filter" % (key, len(issue_list)))
+        env.debug("For component %s, %d issues found after filter" % (self.key, len(issue_list)))
         return issue_list
 
 
