fc3274a8e817c6bd8044f0a2906dffee6e271aec
Olivier Korach <olivier.korach@gmail.com>
2019-11-19 18:07:56 +0100

    SQ API bugs workarounds

--- sonarqube/metrics.py
+++ sonarqube/metrics.py
@@ -2,27 +2,28 @@
 
 import json
 import requests
-import sonarqube.env
+import sonarqube.env as env
 import sonarqube.sqobject
+import sonarqube.utilities as util
 
 
 def get_all_metrics(myenv):
-    parms = dict()
-    if (myenv is None):
-        resp = sonarqube.env.get('/api/metrics/search',  parms)
+    if myenv is None:
+        resp = env.get('/api/metrics/search',  {})
     else:
-        resp = myenv.get('/api/metrics/search', parms)
+        resp = myenv.get('/api/metrics/search', {})
+    if resp.status_code != 200:
+        util.logger.error('HTTP Error %d from SonarQube API query: %s', resp.status_code, resp.content)
     data = json.loads(resp.text)
     return data['metrics']
 
 def get_all_metrics_csv(myenv = None):
     metrics = get_all_metrics(myenv)
-    is_first = True
+    # Workaround for SonarQube Web API bug that does not return vulnerabilities metric
+    csv = 'vulnerabilities'
     for metric in metrics:
-        if is_first:
-            csv_list = metric['key']
-            is_first = False
-        else:
-            csv_list = csv_list + ',' + metric['key']
-    return csv_list
-    
+        if metric['key'] == 'new_development_cost' or metric['key'] == 'vulnerabilities':
+            # Skip new_development_cost metric to work around a SonarQube 7.9 bug
+            continue
+        csv = csv + ",{0}".format(metric['key'])
+    return csv
