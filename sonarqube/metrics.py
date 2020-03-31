620b545239865dd2ea4c7ee4013eb5d1216caf8d
Olivier Korach <olivier.korach@gmail.com>
2020-03-31 15:14:24 +0200

    Added ratings metric support

--- sonarqube/metrics.py
+++ sonarqube/metrics.py
@@ -1,11 +1,16 @@
 #!/usr/local/bin/python3
 
+import re
 import json
 import requests
 import sonarqube.env as env
 import sonarqube.sqobject
 import sonarqube.utilities as util
 
+MAIN_METRICS = 'bugs,reliability_rating,vulnerabilities,security_rating,code_smells,' + \
+    'sqale_rating,sqale_index,coverage,duplicated_lines_density,new_bugs,new_vulnerabilities,new_code_smells,' + \
+    'new_technical_debt,new_maintainability_rating,coverage,duplicated_lines_density,' + \
+    'new_coverage,new_duplicated_lines_density'
 
 def get_all_metrics(myenv):
     # TODO paginated API if more than 500 metrics
@@ -27,3 +32,6 @@ def get_all_metrics_csv(myenv = None):
             continue
         csv = csv + "{0},".format(metric['key'])
     return csv[:-1]
+
+def is_a_rating(metric):
+    return re.match(r"^(new_)?(security|security_review|reliability|maintainability)_rating$", metric)
