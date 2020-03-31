d44ad96c26e164a4ccce98d46fb7f077c17e36f5
Olivier Korach <olivier.korach@gmail.com>
2020-03-31 15:13:58 +0200

    Added branch support

--- sonarqube/measures.py
+++ sonarqube/measures.py
@@ -26,14 +26,31 @@ class Measure (sonarqube.sqobject.SqObject):
         data = json.loads(resp.text)
         return data['component']['measures']
 
-def load_measures(project_key, metrics_list, myenv = None):
+def load_measures(project_key, metrics_list, branch_name = None, sqenv = None):
     parms = dict(component=project_key, metricKeys=metrics_list)
-    if myenv is None:
+    if branch_name is not None:
+        parms['branch'] = branch_name
+    if sqenv is None:
         resp = env.get('/api/measures/component',  parms)
     else:
-        resp = myenv.get('/api/measures/component', parms)
+        resp = sqenv.get('/api/measures/component', parms)
     if resp.status_code != 200:
         util.logger.error('HTTP Error %d from SonarQube API query: %s', resp.status_code, resp.content)
 
     data = json.loads(resp.text)
     return data['component']['measures']
+
+def get_rating_letter(n):
+    if n == '1.0':
+        return 'A'
+    elif n == '2.0':
+        return 'B'
+    elif n == '3.0':
+        return 'C'
+    elif n == '4.0':
+        return 'D'
+    elif n == '5.0':
+        return 'E'
+    else:
+        util.logger.error("Wrong numeric rating provided %s", n)
+        return None
