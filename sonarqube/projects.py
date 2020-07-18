7b8d645d16043db310985712324023869d1bfcfe
Olivier Korach <olivier.korach@gmail.com>
2020-07-18 18:28:32 +0200

    Added export functions (sync/async)

--- sonarqube/projects.py
+++ sonarqube/projects.py
@@ -1,10 +1,12 @@
 #!python3
 
 import sys
+import time
 import json
 import requests
 import sonarqube.env as env
 import sonarqube.components as comp
+import sonarqube.utilities as util
 
 PROJECTS = {}
 class Project(comp.Component):
@@ -21,6 +23,41 @@ class Project(comp.Component):
         resp = self.sqenv.get('/api/project_branches/list', parms=dict(project=self.key))
         data = json.loads(resp.text)
         return data['branches']
+
+    def export(self, poll_interval = 1):
+        util.logger.info('Exporting project key = %s', self.key)
+        resp = self.sqenv.post('/api/project_dump/export', parms={'key':self.key})
+        util.logger.debug('Export response = %s', str(resp))
+        data = json.loads(resp.text)
+        task_id = data['taskId']
+        finished = False
+        parms = {'type':'PROJECT_EXPORT', 'component':self.key, 'status':'PENDING,IN_PROGRESS,SUCCESS,FAILED,CANCELED'}
+        while not finished:
+            time.sleep(poll_interval)
+            resp = self.sqenv.get('/api/ce/activity', parms=parms)
+            data = json.loads(resp.text)
+            for t in data['tasks']:
+                if t['id'] != task_id:
+                    continue
+                status = t['status']
+                if status == 'SUCCESS' or status == 'FAILED' or status == 'CANCELLED':
+                    finished = True
+                    break
+        if status != 'SUCCESS':
+            util.logger.error("Project key %s export %s", self.key, status)
+            return False
+        resp = self.sqenv.get('/api/project_dump/status', parms={'key':self.key})
+        data = json.loads(resp.text)
+        dump_file = data['exportedDump']
+        util.logger.info("Project key %s export %s, dump file %s", self.key, status, dump_file)
+        return dump_file
+
+    def export_async(self):
+        util.logger.info('Exporting project key = %s', self.key)
+        resp = self.sqenv.post('/api/project_dump/export', parms={'key':self.key})
+        util.logger.debug('Export response = %s', str(resp))
+        data = json.loads(resp.text)
+        return data['taskId']
 
 
 def count(include_applications, myenv = None):
