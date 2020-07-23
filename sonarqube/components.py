9b0d4a7ee3ad061f7ad051b15598f8f3382c235b
Olivier Korach <olivier.korach@gmail.com>
2020-07-23 11:59:53 +0200

    reduce get_issues cog complexity

--- sonarqube/components.py
+++ sonarqube/components.py
@@ -51,6 +51,27 @@ class Component:
         ''' Returns the newest date of all issues found '''
         return issues.get_newest_issue(sqenv=self.sqenv, **{'componentKeys': self.key})
 
+    def __get_issues_by_dir__(self, **kwargs):
+        issue_list = []
+        subdirs = issues.get_facets(sqenv = self.sqenv, facet = 'directories', **kwargs)
+        util.logger.debug("Found %d subdirectories", len(subdirs))
+        for subdir in subdirs:
+            kwargs['directories'] = subdir['val']
+            util.logger.debug("Searching issues in sub-component %s / directory %s", \
+                self.key, kwargs['directories'])
+            nbr_issues = self.get_number_of_filtered_issues(**kwargs)
+            if nbr_issues < 10000:
+                issue_list = issue_list + issues.search_all_issues(sqenv = self.sqenv, **kwargs)
+                continue
+            subfiles = issues.get_facets(sqenv = self.sqenv, facet = 'fileUuids', **kwargs)
+            util.logger.debug("Found %d files", len(subfiles))
+            for f in subfiles:
+                kwargs['fileUuids'] = f['val']
+                util.logger.debug("Searching issues in sub-component %s / directory %s / File %s", \
+                    self.key, kwargs['directories'], kwargs['fileUuids'])
+                issue_list = issue_list + issues.search_all_issues(sqenv = self.sqenv, **kwargs)
+        return issue_list
+
     def get_issues(self, **kwargs):
         kwargs['componentKeys'] = self.key
         util.logger.debug(str(kwargs))
@@ -67,8 +88,8 @@ class Component:
         elif 'createdAfter' not in kwargs or 'createdBefore' not in kwargs:
             startdate = datetime.datetime.strptime(oldest, '%Y-%m-%dT%H:%M:%S%z')
             enddate = datetime.datetime.strptime(self.get_newest_issue_date(), '%Y-%m-%dT%H:%M:%S%z')
-            kwargs['createdAfter']  = "%04d-%02d-%02d" % (startdate.year, startdate.month, startdate.day)
-            kwargs['createdBefore'] = "%04d-%02d-%02d" % (enddate.year, enddate.month, enddate.day)
+            kwargs['createdAfter']  = util.format_date(startdate)
+            kwargs['createdBefore'] = util.format_date(enddate)
             issue_list = self.get_issues(**kwargs)
         elif kwargs['createdAfter'] != kwargs['createdBefore']:
             startdate = datetime.datetime.strptime(kwargs['createdAfter'], '%Y-%m-%d')
@@ -76,35 +97,20 @@ class Component:
             diffdays = abs((enddate - startdate).days)
             util.logger.debug("Too many issues, splitting date window [%s - %s] in 2", startdate, enddate)
             mid_date = startdate + datetime.timedelta(days=diffdays//2)
-            kwargs['createdAfter']  = "%04d-%02d-%02d" % (startdate.year, startdate.month, startdate.day)
-            kwargs['createdBefore'] = "%04d-%02d-%02d" % (mid_date.year, mid_date.month, mid_date.day)
+            kwargs['createdAfter']  = util.format_date(startdate)
+            kwargs['createdBefore'] = util.format_date(mid_date)
             util.logger.debug("1st new window: [%s - %s]", kwargs['createdAfter'], kwargs['createdBefore'])
             issue_list = self.get_issues(**kwargs)
             if diffdays <= 1:
                 mid_date = enddate
             else:
                 mid_date = mid_date + datetime.timedelta(days=1)
-            kwargs['createdAfter']  = "%04d-%02d-%02d" % (mid_date.year, mid_date.month, mid_date.day)
-            kwargs['createdBefore'] = "%04d-%02d-%02d" % (enddate.year, enddate.month, enddate.day)
+            kwargs['createdAfter']  = util.format_date(mid_date)
+            kwargs['createdBefore'] = util.format_date(enddate)
             util.logger.debug("2nd new window: [%s - %s]", kwargs['createdAfter'], kwargs['createdBefore'])
             issue_list = issue_list + self.get_issues(**kwargs)
         else:
-            issue_list = []
-            subdirs = issues.get_facets(sqenv = self.sqenv, facet = 'directories', **kwargs)
-            util.logger.debug("Found %d subdirectories", len(subdirs))
-            for subdir in subdirs:
-                kwargs['directories'] = subdir['val']
-                util.logger.debug("Searching issues in sub-component %s / directory %s", self.key, kwargs['directories'])
-                nbr_issues = self.get_number_of_filtered_issues(**kwargs)
-                if nbr_issues < 10000:
-                    issue_list = issue_list + issues.search_all_issues(sqenv = self.sqenv, **kwargs)
-                else:
-                    subfiles = issues.get_facets(sqenv = self.sqenv, facet = 'fileUuids', **kwargs)
-                    util.logger.debug("Found %d files", len(subfiles))
-                    for f in subfiles:
-                        kwargs['fileUuids'] = f['val']
-                        util.logger.debug("Searching issues in sub-component %s / directory %s / File %s", self.key, kwargs['directories'], kwargs['fileUuids'])
-                        issue_list = issue_list + issues.search_all_issues(sqenv = self.sqenv, **kwargs)
+            issue_list = self.__get_issues_by_dir__()
 
         util.logger.info("For component %s, %d issues found after filter", self.key, len(issue_list))
         return issue_list
