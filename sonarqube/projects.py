3c80192f8756d187382ab1398c4e3500c97ea461
Olivier Korach <olivier.korach@gmail.com>
2020-07-23 16:33:53 +0200

    Cleanup

--- sonarqube/projects.py
+++ sonarqube/projects.py
@@ -7,29 +7,30 @@ import sonarqube.env as env
 import sonarqube.components as comp
 
 PROJECTS = {}
+
+PROJECT_SEARCH_API = '/api/projects/search'
+
 class Project(comp.Component):
 
     def get_name(self):
         if self.name is None:
-            params = dict(projects=self.key)
-            resp = requests.get(url=env.get_url() + '/api/projects/search', auth=env.get_credentials(), params=params)
+            resp = self.sqenv.get( PROJECT_SEARCH_API, params={'projects':self.key})
             data = json.loads(resp.text)
             self.name = data['components']['name']
         return self.name
 
     def get_branches(self):
-        resp = self.sqenv.get('/api/project_branches/list', parms=dict(project=self.key))
+        resp = self.sqenv.get('/api/project_branches/list', parms={'project':self.key})
         data = json.loads(resp.text)
         return data['branches']
-
 
 def count(include_applications, myenv = None):
     qualifiers = "TRK,APP" if include_applications else "TRK"
     params = dict(ps=3, qualifiers=qualifiers)
     if myenv is None:
-        resp = env.get('/api/projects/search', params)
+        resp = env.get(PROJECT_SEARCH_API, params)
     else:
-        resp = myenv.get('/api/projects/search', params)
+        resp = myenv.get(PROJECT_SEARCH_API, params)
     data = json.loads(resp.text)
     return data['paging']['total']
 
@@ -37,9 +38,9 @@ def get_projects(include_applications,  myenv = None, page_size=500, page_nbr=1)
     qualifiers = "TRK,APP" if include_applications else "TRK"
     params = dict(ps=page_size, p=page_nbr, qualifiers=qualifiers)
     if myenv is None:
-        resp = env.get('/api/projects/search',  params)
+        resp = env.get(PROJECT_SEARCH_API,  params)
     else:
-        resp = myenv.get('/api/projects/search', params)
+        resp = myenv.get(PROJECT_SEARCH_API, params)
     data = json.loads(resp.text)
     return data['components']
 
@@ -58,9 +59,9 @@ def get_project_name(key, sqenv = None):
 
     params = dict(projects=key)
     if sqenv is None:
-        resp = env.get('/api/projects/search', params)
+        resp = env.get(PROJECT_SEARCH_API, params)
     else:
-        resp = sqenv.get('/api/projects/search', params)
+        resp = sqenv.get(PROJECT_SEARCH_API, params)
     data = json.loads(resp.text)
     #util.json_dump_debug(data)
     PROJECTS[key] = data['components'][0]['name']
