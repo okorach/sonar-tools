690a9122343ecccc449cf407c78ac3a3eaa8d4f6
Olivier Korach <olivier.korach@gmail.com>
2020-07-25 17:37:05 +0200

    inheritance fix

--- sonarqube/components.py
+++ sonarqube/components.py
@@ -4,35 +4,32 @@ import sys
 import datetime
 import json
 import requests
+import sonarqube.sqobject as sq
 import sonarqube.env as env
 import sonarqube.issues as issues
 import sonarqube.measures as measures
 import sonarqube.utilities as util
 
-class Component:
+class Component(sq.SqObject):
 
     def __init__(self, key, name=None, sqenv=None):
-        self.key = key
+        super(Component, self).__init__(key, sqenv)
         self.name = name
-        self.sqenv = sqenv
         self.nbr_issues = None
 
     def get_subcomponents(self):
-        params = dict(component=self.key, strategy='children', ps=500, p=1)
-        if self.sqenv is None:
-            resp = env.get('/api/components/tree', params)
-        else:
-            resp = self.sqenv.get('/api/components/tree', params)
+        params = {'component':self.key, 'strategy':'children', 'ps':500, 'p':1}
+        resp = env.get('components/tree', params, self.env)
         data = json.loads(resp.text)
         comps = []
         for comp in data['components']:
-            comps.append(Component(key=comp['key'], name=comp['name'], sqenv=self.sqenv))
+            comps.append(Component(key=comp['key'], name=comp['name'], sqenv=self.env))
         return comps
 
     def get_number_of_filtered_issues(self, **kwargs):
         kwargs['componentKey'] = self.key
         kwargs['ps'] = 1
-        returned_data = issues.search(sqenv=self.sqenv, **kwargs)
+        returned_data = issues.search(sqenv=self.env, **kwargs)
         return returned_data['total']
 
     def get_number_of_issues(self):
@@ -46,15 +43,15 @@ class Component:
 
     def get_oldest_issue_date(self):
         ''' Returns the oldest date of all issues found '''
-        return issues.get_oldest_issue(sqenv=self.sqenv, **{'componentKeys': self.key})
+        return issues.get_oldest_issue(sqenv=self.env, **{'componentKeys': self.key})
 
     def get_newest_issue_date(self):
         ''' Returns the newest date of all issues found '''
-        return issues.get_newest_issue(sqenv=self.sqenv, **{'componentKeys': self.key})
+        return issues.get_newest_issue(sqenv=self.env, **{'componentKeys': self.key})
 
     def __get_issues_by_dir__(self, **kwargs):
         issue_list = []
-        subdirs = issues.get_facets(sqenv = self.sqenv, facet = 'directories', **kwargs)
+        subdirs = issues.get_facets(sqenv = self.env, facet = 'directories', **kwargs)
         util.logger.debug("Found %d subdirectories", len(subdirs))
         for subdir in subdirs:
             kwargs['directories'] = subdir['val']
@@ -62,15 +59,15 @@ class Component:
                 self.key, kwargs['directories'])
             nbr_issues = self.get_number_of_filtered_issues(**kwargs)
             if nbr_issues < 10000:
-                issue_list = issue_list + issues.search_all_issues(sqenv = self.sqenv, **kwargs)
+                issue_list = issue_list + issues.search_all_issues(sqenv = self.env, **kwargs)
                 continue
-            subfiles = issues.get_facets(sqenv = self.sqenv, facet = 'fileUuids', **kwargs)
+            subfiles = issues.get_facets(sqenv = self.env, facet = 'fileUuids', **kwargs)
             util.logger.debug("Found %d files", len(subfiles))
             for f in subfiles:
                 kwargs['fileUuids'] = f['val']
                 util.logger.debug("Searching issues in sub-component %s / directory %s / File %s", \
                     self.key, kwargs['directories'], kwargs['fileUuids'])
-                issue_list = issue_list + issues.search_all_issues(sqenv = self.sqenv, **kwargs)
+                issue_list = issue_list + issues.search_all_issues(sqenv = self.env, **kwargs)
         return issue_list
 
     def get_issues(self, **kwargs):
@@ -83,7 +80,7 @@ class Component:
         nbr_issues = self.get_number_of_filtered_issues(**kwargs)
         issue_list = []
         if nbr_issues <= 10000:
-            issue_list = issues.search_all_issues(sqenv=self.sqenv, **kwargs)
+            issue_list = issues.search_all_issues(sqenv=self.env, **kwargs)
             for i in issue_list:
                 util.logger.debug(i.to_csv())
         elif 'createdAfter' not in kwargs or 'createdBefore' not in kwargs:
@@ -117,7 +114,7 @@ class Component:
         return issue_list
 
     def get_measure(self, metric_key):
-        for measure in measures.load_measures(self.key, metric_key, sqenv = self.sqenv):
+        for measure in measures.load_measures(self.key, metric_key, sqenv = self.env):
             if 'metric' in measure and metric_key == measure['metric'] and 'value' in measure:
                 return measure['value']
         return None
@@ -125,6 +122,6 @@ class Component:
 
 def get_components(component_types):
     params = dict(ps=500, qualifiers=component_types)
-    resp = requests.get(url=env.get_url() + '/api/projects/search', auth=env.get_credentials(), params=params)
+    resp = env.get('projects/search', params=params)
     data = json.loads(resp.text)
     return data['components']
