93ab9d5f59180752f4e680090ea81d1b59fcd491
Olivier Korach <olivier.korach@gmail.com>
2020-07-31 23:22:12 +0200

    Platform global settings audit

--- sonarqube/env.py
+++ sonarqube/env.py
@@ -6,7 +6,7 @@ import json
 import requests
 import sonarqube.utilities as util
 
-HTTP_ERROR_MSG = "%s%s raised error %s"
+HTTP_ERROR_MSG = "%s%s raised error %d: %s"
 DEFAULT_URL = 'http://localhost:9000'
 
 class Environment:
@@ -78,7 +78,7 @@ class Environment:
             util.logger.error(str(e))
             raise
         if (r.status_code // 100) != 2:
-            util.logger.error(HTTP_ERROR_MSG, self.root_url, api, r.text)
+            util.logger.error(HTTP_ERROR_MSG, self.root_url, api, r.status_code, r.text)
         return r
 
     def post(self, api, params = None):
@@ -93,7 +93,7 @@ class Environment:
             util.logger.error(str(e))
             raise
         if (r.status_code // 100) != 2:
-            util.logger.error(HTTP_ERROR_MSG, self.root_url, api, r.text)
+            util.logger.error(HTTP_ERROR_MSG, self.root_url, api, r.status_code, r.text)
         return r
 
     def delete(self, api, params = None):
@@ -108,7 +108,7 @@ class Environment:
             util.logger.error(str(e))
             raise
         if (r.status_code // 100) != 2:
-            util.logger.error(HTTP_ERROR_MSG, self.root_url, api, r.text)
+            util.logger.error(HTTP_ERROR_MSG, self.root_url, api, r.status_code, r.text)
         return r
 
     def urlstring(self, api, params):
@@ -120,6 +120,44 @@ class Environment:
                 first = False
                 url += '{0}{1}={2}'.format(sep, p, params[p])
         return url
+
+    def __verify_setting__(self, setting, key, value):
+        if setting['key'] == key:
+            if setting['value'] == value:
+                util.logger.info("Setting %s has correct value %s", key, setting['value'])
+                return 0
+            else:
+                util.logger.warning("Setting %s has potentially incorrect/unsafe value %s", key, setting['value'])
+                return 1
+        return 0
+
+    def __verify_project_default_visibility__(self):
+        resp = self.get('navigation/organization', params={'organization':'default-organization'})
+        data = json.loads(resp.text)
+        visi = data['organization']['projectVisibility']
+        if visi == 'private':
+            util.logger.info('Project default visibility is private')
+        else:
+            util.logger.warning('Project default visibility is %s, which can be a security risk', visi)
+            return False
+        return True
+
+    def audit(self):
+        util.logger.info('Auditing global settings')
+        resp = self.get('settings/values')
+        settings = json.loads(resp.text)
+        for s in settings['settings']:
+            self.__verify_setting__(s, 'sonar.forceAuthentication', 'true')
+            self.__verify_setting__(s, 'sonar.cpd.cross_project', 'false')
+            self.__verify_setting__(s, 'sonar.scm.disabled', 'false')
+            # TODO: Check dbCleaner settings
+            # TODO: Check TD rating grip
+            # TODO: Check cost for writing line
+            # TODO: Verify sonar.core.serverBaseURL is set
+        self.__verify_project_default_visibility__()
+
+
+
 
 #--------------------- Static methods, not recommended -----------------
 # this is a pointer to the module object instance itself.
