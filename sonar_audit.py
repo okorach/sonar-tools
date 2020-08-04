9c89abf234409631ff5695911b89cd6fb8ae97ae
Olivier Korach <olivier.korach@gmail.com>
2020-08-04 19:28:55 +0200

    Fixes #36 Fixes #31 Fixes - Cleanup + add QG audit

--- sonar_audit.py
+++ sonar_audit.py
@@ -4,39 +4,33 @@
     Audits a SonarQube platform
 
 '''
+import sys
 import re
-import json
-import datetime
 import argparse
-import requests
-import pytz
-import sonarqube.measures as measures
-import sonarqube.metrics as metrics
 import sonarqube.projects as projects
 import sonarqube.qualityprofiles as qualityprofiles
+import sonarqube.qualitygates as qualitygates
 import sonarqube.utilities as util
 import sonarqube.env as env
 
-def diff(first, second):
-    second = set(second)
-    return [item for item in first if item not in second]
-
 parser = util.set_common_args('Deletes projects not analyzed since a given numbr of days')
-parser.add_argument('-o', '--olderThan', required=True, help='Days since last analysis')
+parser.add_argument('-w', '--what', required=False, help='What to audit settings,projects,qg,qp')
 args = parser.parse_args()
 sq = env.Environment(url=args.url, token=args.token)
 kwargs = vars(args)
 util.check_environment(kwargs)
 
-olderThan = int(args.olderThan)
-if olderThan < 90:
-    util.logger.error("Can't delete projects more recent than 90 days")
-    exit(1)
-issues = projects.audit(endpoint=sq)
-issues += qualityprofiles.audit(endpoint=sq)
-issues += sq.audit()
+issues = 0
+if args.what is None or 'projects' in args.what.split(','):
+    issues += projects.audit(endpoint=sq)
+if args.what is None or 'qp' in args.what.split(','):
+    issues += qualityprofiles.audit(endpoint=sq)
+if args.what is None or 'qg' in args.what.split(','):
+    issues += qualitygates.audit(endpoint=sq)
+if args.what is None or 'settings' in args.what.split(','):
+    issues += sq.audit()
 if issues > 0:
     util.logger.warning("%d issues found during audit", issues)
 else:
     util.logger.info("%d issues found during audit", issues)
-exit(1)
+sys.exit(issues)
