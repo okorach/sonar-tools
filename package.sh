84fb0e3b8bf923106008c808c7dca6c8d6fd61d6
Olivier Korach <olivier.korach@gmail.com>
2020-08-23 22:42:12 +0200

    Add install in packaging

--- package.sh
+++ package.sh
@@ -3,6 +3,9 @@
 rm -rf build dist
 python3 setup.py bdist_wheel
 
-# python3 -p pip uninstall sonar-tools
-# python3 -m pip install dist/*-py3-*.whl
-# python3 -m twine upload dist/*
+python3 -m pip uninstall sonar-tools
+python3 -m pip install dist/*-py3-*.whl
+
+if [ "$1" = "publish" ]; then
+    python3 -m twine upload dist/*
+fi
