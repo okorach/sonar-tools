813301fd000d3d715463211b459f2f48a634f329
Olivier Korach <olivier.korach@gmail.com>
2021-12-22 16:29:23 +0100

    allow to send loc to CSV file

--- sonarqube/loc.py
+++ sonarqube/loc.py
@@ -26,6 +26,42 @@ from sonarqube import projects, env, version
 import sonarqube.utilities as util
 
 
+def __dump_loc(projects, file, file_format, **kwargs):
+    if file is None:
+        fd = sys.stdout
+        util.logger.info("Dumping LoC report to stdout")
+    else:
+        fd = open(file, "w", encoding='utf-8')
+        util.logger.info("Dumping LoC report to file '%s'", file)
+    sep = ','
+
+    print("# Project Key", end='', file=fd)
+    if kwargs['projectName']:
+        print(f"{sep}Project Name", end='', file=fd)
+    print(f"{sep}LoC", end='', file=fd)
+    if kwargs['lastAnalysis']:
+        print(f"{sep}Last Analysis", end='', file=fd)
+    print('', file=fd)
+    nb_loc = 0
+    nb_projects = 0
+    for _, p in projects.items():
+        project_loc = p.ncloc(include_branches=True)
+        print(f"{p.key}", end='', file=fd)
+        if kwargs['projectName']:
+            print(f"{sep}{p.name}", end='', file=fd)
+        print(f"{sep}{project_loc}", end='', file=fd)
+        if kwargs['lastAnalysis']:
+            print(f"{sep}{p.last_analysis_date(include_branches=True)}", end='', file=fd)
+        print('', file=fd)
+        nb_loc += project_loc
+        nb_projects += 1
+        if nb_projects % 50 == 0:
+            util.logger.info("%d PROJECTS and %d LoCs, still counting...", nb_projects, nb_loc)
+    if file is not None:
+        fd.close()
+    util.logger.info("%d PROJECTS and %d LoCs", len(projects), nb_loc)
+
+
 def main():
     parser = util.set_common_args('Extract projects lines of code, as computed for the licence')
     parser = util.set_component_args(parser)
@@ -33,34 +69,21 @@ def main():
                         help='Also list the project name on top of the project key')
     parser.add_argument('-a', '--lastAnalysis', required=False, default=False, action='store_true',
                         help='Also list the last analysis date on top of nbr of LoC')
+    parser.add_argument('-o', '--outputFile', required=False, help='File to generate the report, default is stdout'
+                        'Format is automatically deducted from file extension, if extension given')
+    parser.add_argument('-f', '--format', required=False, default='csv',
+                        help='Format of output (json, csv), default is csv')
     args = util.parse_and_check_token(parser)
     endpoint = env.Environment(url=args.url, token=args.token)
     util.check_environment(vars(args))
     util.logger.info('sonar-tools version %s', version.PACKAGE_VERSION)
 
     # Mandatory script input parameters
-    sep = ","
-    print("# Project Key", end='')
-    if args.projectName:
-        print(f"{sep}Project Name", end='')
-    print(f"{sep}LoC", end='')
-    if args.lastAnalysis:
-        print(f"{sep}Last Analysis", end='')
-    print('')
+
+
 
     project_list = projects.search(endpoint=endpoint)
-    nb_loc = 0
-    for _, p in project_list.items():
-        project_loc = p.ncloc(include_branches=True)
-        print(f"{p.key}", end='')
-        if args.projectName:
-            print(f"{sep}{p.name}", end='')
-        print(f"{sep}{project_loc}", end='')
-        if args.lastAnalysis:
-            print(f"{sep}{p.last_analysis_date(include_branches=True)}", end='')
-        print('')
-        nb_loc += project_loc
-    util.logger.info("%d PROJECTS and %d LoCs", len(project_list), nb_loc)
+    __dump_loc(project_list, args.outputFile, args.format, **vars(args))
     sys.exit(0)
 
 
