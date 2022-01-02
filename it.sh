47958f56606841c583d59a87dec73b8ca191ed30
Olivier Korach <olivier.korach@gmail.com>
2022-01-02 17:02:56 +0100

    Add comparison of tools results with released version

--- it.sh
+++ it.sh
@@ -1,50 +1,76 @@
 
 set -euo pipefail
 
-echo "Hello $*"
-
 [ $# -eq 0 ] && echo "Usage: $0 <env1> [... <envN>]" && exit 1
 
-rm -f *.json *.csv
+mkdir -p tmp
+rm -f tmp/*
 
+echo "Restore sonar-tools last released version"
+echo "Y" | pip uninstall sonar-tools
+pip install sonar-tools
+for env in $*
+do
+    sonar-measures-export -b -o tmp/measures-$env-released.csv -m _main
+    sonar-issues-export -o tmp/issues-$env-released.csv
+    sonar-audit || echo "OK" >tmp/audit-$env-released.csv
+    sonar-loc -n -a >tmp/loc-$env-released.csv 
+done
+
+echo "Install sonar-tools current local version"
+./deploy.sh
 for env in $*
 do
     echo "Running with environment $env"
     . sqenv $env
-    sonar-measures-export -b -o measures-$env-1.csv -m _all
-    [ -s "measures-$env-1.csv" ]
-    sonar-measures-export -b -p -r -d -m _all >measures-$env-2.csv
-    [ -s "measures-$env-2.csv" ]
-    sonar-measures-export -b -o measures-$env-1.json -m _all
-    [ -s "measures-$env-1.json" ]
-    sonar-measures-export -b -p -r -d -m _all -f json >measures-$env-2.json
-    [ -s "measures-$env-2.json" ]
+    sonar-measures-export -b -o tmp/measures-$env-unreleased.csv -m _main
+    [ -s "tmp/measures-$env-unreleased.csv" ]
+    sonar-measures-export -b -p -r -d -m _all >tmp/measures-$env-2.csv
+    [ -s "tmp/measures-$env-2.csv" ]
+    sonar-measures-export -b -o tmp/measures-$env-1.json -m _all
+    [ -s "tmp/measures-$env-1.json" ]
+    sonar-measures-export -b -p -r -d -m _all -f json >tmp/measures-$env-2.json
+    [ -s "tmp/measures-$env-2.json" ]
 
-    sonar-issues-export -o issues-$env-1.csv
-    [ -s "issues-$env-1.csv" ]
-    sonar-issues-export -o issues-$env-1.json
-    [ -s "issues-$env-1.json" ]
-    sonar-issues-export -f json -k okorach_audio-video-tools,okorach_sonarqube-tools >issues-$env-2.json
-    [ -s "issues-$env-2.json" ]
-    sonar-issues-export -f json -k okorach_audio-video-tools,okorach_sonarqube-tools --useFindings >issues-$env-3.json
-    [ -s "issues-$env-3.json" ]
+    sonar-issues-export -o tmp/issues-$env-unreleased.csv
+    [ -s "tmp/issues-$env-unreleased.csv" ]
+    sonar-issues-export -o tmp/issues-$env-1.json
+    [ -s "tmp/issues-$env-1.json" ]
+    sonar-issues-export -f json -k okorach_audio-video-tools,okorach_sonarqube-tools >tmp/issues-$env-2.json
+    [ -s "tmp/issues-$env-2.json" ]
+    sonar-issues-export -f json -k okorach_audio-video-tools,okorach_sonarqube-tools --useFindings >tmp/issues-$env-3.json
+    [ -s "tmp/issues-$env-3.json" ]
 
-    sonar-audit || echo "OK" >audit-$env-1.csv
-    [ -s "audit-$env-1.csv" ]
-    sonar-audit -f audit-$env-1.json || echo "OK"
-    [ -s "audit-$env-1.json" ]
-    sonar-audit --format json --what qp,qg,settings || echo "OK" >audit-$env-2.json
-    [ -s "audit-$env-2.json" ]
+    sonar-audit || echo "OK" >tmp/audit-$env-unreleased.csv
+    [ -s "tmp/audit-$env-unreleased.csv" ]
+    sonar-audit -f tmp/audit-$env-1.json || echo "OK"
+    [ -s "tmp/audit-$env-1.json" ]
+    sonar-audit --format json --what qp,qg,settings || echo "OK" >tmp/audit-$env-2.json
+    [ -s "tmp/audit-$env-2.json" ]
 
-    sonar-housekeeper -P 365 -B 90 -T 180 -R 30 >housekeeper-$env-1.csv || echo "OK"
-    [ -s "housekeeper-$env-1.csv" ]
+    sonar-housekeeper -P 365 -B 90 -T 180 -R 30 >tmp/housekeeper-$env-1.csv || echo "OK"
+    [ -s "tmp/housekeeper-$env-1.csv" ]
 
-    sonar-loc >loc-$env-1.csv
-    [ -s "loc-$env-1.csv" ]
-    sonar-loc -n -a >loc-$env-2.csv 
-    [ -s "loc-$env-2.csv" ]
+    sonar-loc >tmp/loc-$env-1.csv
+    [ -s "tmp/loc-$env-1.csv" ]
+    sonar-loc -n -a >tmp/loc-$env-unreleased.csv 
+    [ -s "tmp/loc-$env-unreleased.csv" ]
 
     sonar-projects-export
+done
+
+rm -f diff.txt
+for env in $*
+do
+    for f in measures issues audit loc
+    do
+        echo "==========================" | tee -a diff.txt
+        echo $f-$env diff                 | tee -a diff.txt
+        echo "==========================" | tee -a diff.txt
+        sort tmp/$f-$env-released.csv >tmp/$f-$env-released.sorted.csv
+        sort tmp/$f-$env-unreleased.csv >tmp/$f-$env-unreleased.sorted.csv
+        diff tmp/$f-$env-released.sorted.csv tmp/$f-$env-unreleased.sorted.csv | tee -a diff.txt
+    done
 done
 
 echo "====================================="
