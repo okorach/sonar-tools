f152347a6cfa161bd4a5e922de3e3ac46cee24dd
Olivier Korach <olivier.korach@gmail.com>
2022-01-30 09:52:13 +0100

    Remove one branch useless for tests

--- tests-it/test-sync.sh
+++ tests-it/test-sync.sh
@@ -3,7 +3,7 @@ do
 	curl -X POST -u $SONAR_TOKEN: "$SONAR_HOST_URL/api/projects/delete?project=$proj"
 	opts="-Dsonar.projectKey=$proj -Dsonar.projectName=$proj"
 	scan.sh $opts
-	for branch in release-1.x hotfix
+	for branch in release-1.x
 	do
 		scan.sh $* $opts -Dsonar.branch.name=$branch
 	done
