e3c06b9cce295d6f4ee79912c1fbdd38337579cd
Olivier Korach <olivier.korach@gmail.com>
2022-01-30 09:37:56 +0100

    Delete project before recreate

--- tests-it/test-sync.sh
+++ tests-it/test-sync.sh
@@ -1,5 +1,6 @@
 for proj in source target
 do
+	curl -X POST -u $SONAR_TOKEN: "$SONAR_HOST_URL/api/projects/delete?project=$proj"
 	opts="-Dsonar.projectKey=$proj -Dsonar.projectName=$proj"
 	scan.sh $opts
 	for branch in release-1.x hotfix
