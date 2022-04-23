510f966c0e20629f1964e0b6506ea21a60700b6e
Olivier Korach <olivier.korach@gmail.com>
2022-04-23 10:49:50 +0200

    Count settings in categories

--- sonar/config.py
+++ sonar/config.py
@@ -56,9 +56,13 @@ def main():
     args = __parse_args('Extract SonarQube platform configuration')
     endpoint = env.Environment(some_url=args.url, some_token=args.token)
 
-    platform_settings = endpoint.settings()
+    platform_settings = endpoint.settings(include_not_set=True)
     print(util.json_dump(platform_settings))
-    util.logger.info("Exported %s settings", len(platform_settings))
+    nbr_settings = len(platform_settings)
+    for categ in ('languages', 'auth', 'tests'):
+        if categ in platform_settings:
+            nbr_settings += len(platform_settings[categ]) - 1
+    util.logger.info("Exported %s settings", nbr_settings)
     sys.exit(0)
 
 
