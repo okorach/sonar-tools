b380068e29a3c7ac709f894f8f9000f35d453dc3
Olivier Korach <olivier.korach@gmail.com>
2022-05-18 20:47:39 +0200

    Fixes #535

--- sonar/qualityprofiles.py
+++ sonar/qualityprofiles.py
@@ -152,9 +152,10 @@ class QualityProfile(sq.SqObject):
                 'projectsCount': self.project_count,
                 'deprecatedRulesCount': self.nbr_deprecated_rules,
                 'lastUsed': self.last_used,
-                'parentProfileKey': self.parent_key,
-                'parentProfileName': self.parent_name
             })
+        if self.parent_key is not None:
+            json_data['parentName'] = self.parent_name
+            json_data['parentKey'] = self.parent_key
         perms = self.permissions()
         if perms is not None and len(perms) > 0:
             json_data['permissions'] = perms
@@ -278,7 +279,6 @@ def get_list(endpoint=None, include_rules=False):
     util.logger.info("Exporting quality profiles")
     for qp in _QUALITY_PROFILES.values():
         json_data = qp.to_json(include_rules=True)
-        json_data.pop('language')
         json_data.pop('name')
         qp_list[f"{qp.language}{settings.UNIVERSAL_SEPARATOR}{qp.name}"] = json_data
     return qp_list
