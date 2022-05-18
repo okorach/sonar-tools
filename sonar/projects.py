545d1497b3eeb4e575d89b1643c197774e5de1a4
Olivier Korach <olivier.korach@gmail.com>
2022-05-18 22:14:41 +0200

    Fixes #471

--- sonar/projects.py
+++ sonar/projects.py
@@ -595,6 +595,13 @@ Is this normal ?", gr['name'], str(self.key))
             link_list.append({'type': link['type'], 'url': link['url']})
         return link_list
 
+    def webhooks(self):
+        data = json.loads(self.get('webhooks/list', params={'project': self.key}).text)
+        if len(data.get('webhooks', [])) > 0:
+            return data['webhooks']
+        else:
+            return None
+
     def settings(self, settings_list=None, format='json', include_inherited=False):
         util.logger.info("Exporting settings for %s", str(self))
         settings_dict = settings.get_bulk(endpoint=self, project=self, settings_list=settings_list, include_not_set=False)
@@ -630,6 +637,14 @@ Is this normal ?", gr['name'], str(self.key))
         p_links = self.links()
         if p_links is not None:
             json_data['links'] = p_links
+        whooks = self.webhooks()
+        if whooks is not None:
+            for wh in whooks:
+                wh.pop('key', None)
+                wh.pop('latestDelivery', None)
+            if settings.GENERAL_SETTINGS not in json_data:
+                json_data[settings.GENERAL_SETTINGS] = {}
+            json_data[settings.GENERAL_SETTINGS].update({'webhooks': whooks})
         util.json_dump_debug(json_data, f"PROJECT {self.key}:")
         return json_data
 
