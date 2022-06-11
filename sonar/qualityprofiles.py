42bb5710d92feb8a52ebedbc52387098bcf02df3
Olivier Korach <olivier.korach@gmail.com>
2022-06-11 17:12:08 +0200

    Add exists()

--- sonar/qualityprofiles.py
+++ sonar/qualityprofiles.py
@@ -486,3 +486,6 @@ def name_to_uuid(name, lang):
 
 def _uuid(key):
     return key
+
+def exists(language, name, endpoint):
+    return get_object(name=name, language=language, endpoint=endpoint) is not None
