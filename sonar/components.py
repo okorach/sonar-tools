fa0ea30c5efd072c118664a87af92971cdc78017
Olivier Korach <olivier.korach@gmail.com>
2022-07-02 09:22:04 +0200

    Fix bug post instead of get

--- sonar/components.py
+++ sonar/components.py
@@ -63,7 +63,7 @@ class Component(sq.SqObject):
         elif self._json is not None and "tags" in self._json:
             self._tags = self._json["tags"]
         else:
-            data = json.loads(self.post(_DETAILS_API, params={"component": self.key}).text)
+            data = json.loads(self.get(_DETAILS_API, params={"component": self.key}).text)
             if self._json is None:
                 self._json = data["component"]
             else:
