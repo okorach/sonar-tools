1688baeea78b01378b3a76e934e8a8d0ab6aa383
Olivier Korach <olivier.korach@gmail.com>
2022-07-04 12:15:10 +0200

    Threaded object search

--- sonar/sqobject.py
+++ sonar/sqobject.py
@@ -24,6 +24,8 @@
 """
 
 import json
+from queue import Queue
+from threading import Thread
 from sonar import utilities
 
 
@@ -53,25 +55,50 @@ class SqObject:
         return resp.ok
 
 
-def search_objects(api, endpoint, key_field, returned_field, object_class, params):
+def __search_thread(queue, foo):
+    while not queue.empty():
+        (endpoint, api, objects, key_field, returned_field, object_class, params, page) = queue.get()
+        page_params = params.copy()
+        page_params["page"] = page
+        utilities.logger.debug("Threaded search: API = %s params = %s", api, str(params))
+        data = json.loads(endpoint.get(api, params=params).text)
+        for obj in data[returned_field]:
+            if object_class.__name__ in ("Portfolio", "Groups"):
+                objects[obj[key_field]] = object_class.load(name=obj[key_field], endpoint=endpoint, data=obj)
+            elif object_class.__name__ == "QualityProfile":
+                objects[obj[key_field]] = object_class.load(name=obj["name"], language=obj["language"], endpoint=endpoint, data=obj)
+            else:
+                objects[obj[key_field]] = object_class(obj[key_field], endpoint, data=obj)
+        queue.task_done()
+
+
+def search_objects(api, endpoint, key_field, returned_field, object_class, params, threads=8):
     __MAX_SEARCH = 500
     new_params = {} if params is None else params.copy()
     if "ps" not in new_params:
         new_params["ps"] = __MAX_SEARCH
-    page, nb_pages = 1, 1
+    new_params["p"] = 1
     objects_list = {}
-    while page <= nb_pages:
-        new_params["p"] = page
-        data = json.loads(endpoint.get(api, params=new_params).text)
-        for obj in data[returned_field]:
-            if object_class.__name__ in ("Portfolio", "Groups"):
-                objects_list[obj[key_field]] = object_class.load(name=obj[key_field], endpoint=endpoint, data=obj)
-            elif object_class.__name__ == "QualityProfile":
-                objects_list[obj[key_field]] = object_class.load(name=obj["name"], language=obj["language"], endpoint=endpoint, data=obj)
-            else:
-                objects_list[obj[key_field]] = object_class(obj[key_field], endpoint, data=obj)
-        nb_pages = utilities.nbr_pages(data)
-        page += 1
+    data = json.loads(endpoint.get(api, params=new_params).text)
+    for obj in data[returned_field]:
+        if object_class.__name__ in ("Portfolio", "Groups"):
+            objects_list[obj[key_field]] = object_class.load(name=obj[key_field], endpoint=endpoint, data=obj)
+        elif object_class.__name__ == "QualityProfile":
+            objects_list[obj[key_field]] = object_class.load(name=obj["name"], language=obj["language"], endpoint=endpoint, data=obj)
+        else:
+            objects_list[obj[key_field]] = object_class(obj[key_field], endpoint, data=obj)
+    nb_pages = utilities.nbr_pages(data)
+    if nb_pages == 1:
+        return objects_list
+    q = Queue(maxsize=0)
+    for page in range(2, nb_pages+1):
+        q.put((endpoint, api, objects_list, key_field, returned_field, object_class, params, page))
+    for i in range(threads):
+        utilities.logger.debug("Starting %s search thread %d", object_class.__name__, i)
+        worker = Thread(target=__search_thread, args=(q, "foo"))
+        worker.setDaemon(True)
+        worker.start()
+    q.join()
     return objects_list
 
 
