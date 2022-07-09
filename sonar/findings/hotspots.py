d1725dfa02fde465e3f8d19720908b4b042cf5fa
Olivier Korach <olivier.korach@gmail.com>
2022-07-09 11:58:07 +0200

    Fix project scope

--- sonar/findings/hotspots.py
+++ sonar/findings/hotspots.py
@@ -23,7 +23,8 @@ import json
 import re
 import requests.utils
 import sonar.utilities as util
-from sonar import projects, syncer, users
+from sonar import syncer, users
+from sonar.projects import projects
 from sonar.findings import findings, changelog
 
 SEARCH_CRITERIAS = (
