f8fd54c7d0cf49cd165d262a79e2639b49ca37f7
Olivier Korach <olivier.korach@gmail.com>
2022-07-10 16:14:36 +0200

    Refactoring

--- sonar/metrics.py
+++ sonar/metrics.py
@@ -22,56 +22,50 @@
     Abstraction of the SonarQube "metric" concept
 
 """
-import re
-import json
-import sonar.sqobject as sq
-import sonar.utilities as util
 
+import json
+from sonar import sqobject, utilities
+
+MAIN_METRICS = (
+    "bugs",
+    "vulnerabilities",
+    "code_smells",
+    "security_hotspots",
+    "reliability_rating",
+    "security_rating",
+    "sqale_rating",
+    "security_review_rating",
+    "sqale_debt_ratio",
+    "coverage",
+    "duplicated_lines_density",
+    "security_hotspots_reviewed",
+    "new_bugs",
+    "new_vulnerabilities",
+    "new_code_smells",
+    "new_security_hotspots",
+    "new_reliability_rating",
+    "new_security_rating",
+    "new_maintainability_rating",
+    "new_security_review_rating",
+    "new_sqale_debt_ratio",
+    "new_coverage",
+    "new_duplicated_lines_density",
+    "new_security_hotspots_reviewed",
+    "ncloc",
+)
+
+METRICS_BY_TYPE = {}
+
+APIS = {
+    "search": "metrics/search",
+}
 __MAX_PAGE_SIZE = 500
 
+_OBJECTS = {}
+_VISIBLE_OBJECTS = {}
 
-class Metric(sq.SqObject):
+class Metric(sqobject.SqObject):
     Count = None
-    Inventory = {}
-    SEARCH_API = "metrics/search"
-    MAIN_METRICS = (
-        "bugs",
-        "vulnerabilities",
-        "code_smells",
-        "security_hotspots",
-        "reliability_rating",
-        "security_rating",
-        "sqale_rating",
-        "security_review_rating",
-        "sqale_debt_ratio",
-        "coverage",
-        "duplicated_lines_density",
-        "security_hotspots_reviewed",
-        "new_bugs",
-        "new_vulnerabilities",
-        "new_code_smells",
-        "new_security_hotspots",
-        "new_reliability_rating",
-        "new_security_rating",
-        "new_maintainability_rating",
-        "new_security_review_rating",
-        "new_sqale_debt_ratio",
-        "new_coverage",
-        "new_duplicated_lines_density",
-        "new_security_hotspots_reviewed",
-        "ncloc",
-    )
-
-    RATING_METRICS = (
-        "sqale_rating",
-        "new_maintainability_rating",
-        "security_rating",
-        "new_security_rating",
-        "reliability_rating",
-        "new_reliability_rating",
-        "security_review_rating",
-        "new_security_review_rating",
-    )
 
     def __init__(self, key=None, endpoint=None, data=None):
         super().__init__(key, endpoint)
@@ -83,19 +77,20 @@ class Metric(sq.SqObject):
         self.qualitative = None
         self.hidden = None
         self.custom = None
-        self.__load__(data)
+        self.__load(data)
+        _OBJECTS[self.key] = self
 
-    def __load__(self, data):
+    def __load(self, data):
         if data is None:
             # TODO handle pagination
-            data_json = json.loads(self.get(Metric.SEARCH_API, params={"ps": 500}).text)
+            data_json = json.loads(self.get(APIS["search"], params={"ps": 500}).text)
             for m in data_json["metrics"]:
                 if self.key == m["key"]:
                     data = m
                     break
         if data is None:
             return False
-        util.logger.debug("Loading metric %s", str(data))
+        utilities.logger.debug("Loading metric %s", str(data))
         self.type = data["type"]
         self.name = data["name"]
         self.description = data.get("description", "")
@@ -103,60 +98,46 @@ class Metric(sq.SqObject):
         self.qualitative = data["qualitative"]
         self.hidden = data["hidden"]
         self.custom = data.get("custom", None)
+        if not self.hidden:
+            _VISIBLE_OBJECTS[self.key] = self
+        if self.type not in METRICS_BY_TYPE:
+            METRICS_BY_TYPE[self.type] = set()
+            METRICS_BY_TYPE[self.type].add(self.key)
         return True
 
     def is_a_rating(self):
-        return re.match(
-            r"^(new_)?(security|security_review|reliability|maintainability)_rating$",
-            self.key,
-        )
+        return self.type == "RATING"
 
+    def is_a_percent(self):
+        return self.type == "PERCENT"
+
+def is_of_type(metric_key, metric_type):
+    return metric_key in METRICS_BY_TYPE[metric_key]
+
+def is_a_rating(metric_key):
+    return is_of_type(metric_key, "RATING")
+
+def is_a_percent(metric_key):
+    return is_of_type(metric_key, "PERCENT")
 
 def count(endpoint):
-    if Metric.Count is None:
-        data = json.loads(endpoint.get(Metric.SEARCH_API, params={"ps": 1}).text)
-        Metric.Count = data["total"]
-    return Metric.Count
+    if len(_OBJECTS) is None:
+        search(endpoint, True)
+    return len(_VISIBLE_OBJECTS)
 
-
-def search(endpoint, skip_hidden_metrics=True):
-    if not Metric.Inventory:
+def search(endpoint, show_hidden_metrics=False):
+    if len(_OBJECTS) == 0:
         m_list = {}
         page, nb_pages = 1, 1
         while page <= nb_pages:
-            data = json.loads(
-                endpoint.get(
-                    Metric.SEARCH_API,
-                    params={"ps": __MAX_PAGE_SIZE, "p": page},
-                ).text
-            )
+            data = json.loads( endpoint.get(APIS["search"], params={"ps": __MAX_PAGE_SIZE, "p": page}).text)
             for m in data["metrics"]:
                 m_list[m["key"]] = Metric(key=m["key"], endpoint=endpoint, data=m)
-            nb_pages = util.nbr_pages(data)
+            nb_pages = utilities.nbr_pages(data)
             page += 1
-        Metric.Inventory = m_list
 
-    if not skip_hidden_metrics:
-        return m_list
-    final_list = m_list.copy()
-    for k, v in m_list.items():
-        if v.hidden:
-            final_list.pop(k)
-    return final_list
+    return _OBJECTS if show_hidden_metrics else _VISIBLE_OBJECTS
 
 
 def as_csv(metric_list, separator=","):
-    csv = ""
-    for metric in metric_list:
-        if metric.key == "new_development_cost":
-            # Skip new_development_cost metric to work around a SonarQube 7.9 bug
-            continue
-        csv = csv + f"{metric.key}{separator}"
-    return csv[:-1]
-
-
-def is_a_rating(metric):
-    return re.match(
-        r"^(new_)?(security|security_review|reliability|maintainability)_rating$",
-        metric,
-    )
+    return separator.join([m.key for m in metric_list if m.key != "new_development_cost"])
