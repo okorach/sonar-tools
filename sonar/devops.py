4925de8903ef80ae7b95f91cab0a2b3c1ce9af8a
Olivier Korach <olivier.korach@gmail.com>
2022-07-12 10:25:48 +0200

    Refactoring

--- sonar/devops.py
+++ sonar/devops.py
@@ -21,10 +21,16 @@
 
 from http import HTTPStatus
 import json
-from sonar import sqobject
+
+from requests.exceptions import HTTPError
+
+from sonar import sqobject, exceptions
 import sonar.utilities as util
 
-_DEVOPS_PLATFORM_TYPES = ("github", "azure", "bitbucket", "bitbucketcloud", "gitlab")
+#: DevOps platform types in SonarQube
+DEVOPS_PLATFORM_TYPES = ("github", "azure", "bitbucket", "bitbucketcloud", "gitlab")
+
+
 _OBJECTS = {}
 
 _CREATE_API_GITHUB = "alm_settings/create_github"
@@ -32,7 +38,7 @@ _CREATE_API_GITLAB = "alm_settings/create_gitlab"
 _CREATE_API_AZURE = "alm_settings/create_azure"
 _CREATE_API_BITBUCKET = "alm_settings/create_bitbucket"
 _CREATE_API_BBCLOUD = "alm_settings/create_bitbucketcloud"
-_LIST_API = "alm_settings/list_definitions"
+APIS = {"list": "alm_settings/list_definitions"}
 
 _IMPORTABLE_PROPERTIES = ("key", "type", "url", "workspace", "clientId", "appId")
 
@@ -42,63 +48,89 @@ class DevopsPlatform(sqobject.SqObject):
     Abstraction of the SonarQube ALM/DevOps Platform concept
     """
 
-    def __init__(self, key, devops_platform_type, endpoint, data=None, create_data=None):
-        super().__init__(key, endpoint)
-        self.type = devops_platform_type  #: DevOps platform type
-        if create_data is not None:
-            exit_on_error = self.endpoint.edition() in ("enterprise", "datacenter")
-            self.type = create_data.pop("type")
-            params = create_data
-            params["key"] = self.key
-            if self.type == "github":
+    @classmethod
+    def read(cls, endpoint, key):
+        if key in _OBJECTS:
+            return _OBJECTS[key]
+        data = json.loads(endpoint.get(APIS["list"]).text)
+        for plt_type, platforms in data.items():
+            for p in platforms:
+                if p["key"] == key:
+                    return cls.load(endpoint, plt_type, data)
+        raise exceptions.ObjectNotFound(f"DevOps platform key '{key}' not found")
+
+    @classmethod
+    def load(cls, endpoint, plt_type, data):
+        key = data["key"]
+        if key in _OBJECTS:
+            return _OBJECTS[key]
+        o = DevopsPlatform(key, endpoint, plt_type)
+        o._load(data)
+
+    @classmethod
+    def create(cls, endpoint, key, plt_type):
+        # exit_on_error = self.endpoint.edition() in ("enterprise", "datacenter")
+        params = {"key": key}
+        try:
+            if plt_type == "github":
                 params["clientSecret"] = "TO_BE_SET"
                 params["privateKey"] = "TO_BE_SET"
-                r = self.post(_CREATE_API_GITHUB, params=params, exit_on_error=exit_on_error)
-            elif self.type == "azure":
+                r = endpoint.post(_CREATE_API_GITHUB, params=params)
+            elif plt_type == "azure":
                 # TODO: pass secrets on the cmd line
                 params["personalAccessToken"] = "TO_BE_SET"
-                r = self.post(_CREATE_API_AZURE, params=params, exit_on_error=exit_on_error)
-            elif self.type == "gitlab":
+                r = endpoint.post(_CREATE_API_AZURE, params=params)
+            elif plt_type == "gitlab":
                 params["personalAccessToken"] = "TO_BE_SET"
-                r = self.post(_CREATE_API_GITLAB, params=params, exit_on_error=exit_on_error)
-            elif self.type == "bitbucket":
+                r = endpoint.post(_CREATE_API_GITLAB, params=params)
+            elif plt_type == "bitbucket":
                 params["personalAccessToken"] = "TO_BE_SET"
-                r = self.post(_CREATE_API_BITBUCKET, params=params, exit_on_error=exit_on_error)
-            elif self.type == "bitbucketcloud":
+                r = endpoint.post(_CREATE_API_BITBUCKET, params=params)
+            elif plt_type == "bitbucketcloud":
                 params["clientSecret"] = "TO_BE_SET"
-                r = self.post(_CREATE_API_BBCLOUD, params=params, exit_on_error=exit_on_error)
-            if r.status_code == HTTPStatus.BAD_REQUEST and self.endpoint.edition() == "developer":
-                util.logger.warning("Can't set DevOps platform %s, don't you have more that 1 of that type?", self.key)
-            else:
-                self.read()
-        elif data is None:
-            self.read()
-        else:
-            self._json = data
-            self.url = data.get("url", "")  #: DevOps platform base URL
-        if devops_platform_type == "bitbucketcloud":
-            self.url = "https://bitbucket.org"
+                r = endpoint.post(_CREATE_API_BBCLOUD, params=params)
+        except HTTPError as e:
+            if e.response.status_code == HTTPStatus.BAD_REQUEST and endpoint.edition() == "developer":
+                util.logger.warning("Can't set DevOps platform '%s', don't you have more that 1 of that type?", key)
+                raise exceptions.UnsupportedOperation(f"Can't set DevOps platform '{key}', don't you have more that 1 of that type?")
+        o = DevopsPlatform(key, endpoint, plt_type)
+        o.refresh()
+        return o
+
+    def __init__(self, key, endpoint, platform_type):
+        super().__init__(key, endpoint)
+        self.type = platform_type  #: DevOps platform type
+        self.url = None  #: DevOps platform URL
+        self._specific = None  #: DevOps platform specific settings
         _OBJECTS[key] = self
-        util.logger.debug("Created %s", str(self))
+        util.logger.debug("Created object %s", str(self))
+
+    def _load(self, data):
+        self._json = data
+        self.url = "https://bitbucket.org" if self.type == "bitbucketcloud" else data["url"]
+        self._specific = data.copy()
+        for k in ("key", "url"):
+            self._specific.pop(k, None)
+        return self
 
     def __str__(self):
         string = f"devops platform '{self.key}'"
-        if self.type == "bitbucketcloud":
-            string += f" workspace '{self._json['workspace']}'"
+        if self.type == "bitbucketcloud" and self._specific:
+            string += f" workspace '{self._specific['workspace']}'"
         return string
 
-    def read(self):
+    def refresh(self):
         """Reads / Refresh a DevOps platform information
 
-        :return: Nothing
+        :return: Whether the operation succeeded
+        :rtype: bool
         """
-        data = json.loads(self.get(_LIST_API).text)
+        data = json.loads(self.get(APIS["list"]).text)
         for alm_data in data.get(self.type, {}):
             if alm_data["key"] != self.key:
-                continue
-            self._json = alm_data
-            break
-        return self._json
+                self._json = alm_data
+                return True
+        return False
 
     def to_json(self, full=False):
         """Exports a DevOps platform configuration in JSON format
@@ -112,7 +144,13 @@ class DevopsPlatform(sqobject.SqObject):
         json_data.update({"key": self.key, "type": self.type, "url": self.url})
         return util.filter_export(json_data, _IMPORTABLE_PROPERTIES, full)
 
-    def update(self, data):
+    def set_pat(self, pat, user_name=None):
+        if self.type == "github":
+            util.logger.warning("Can't set PAT for GitHub devops platform")
+            return False
+        return self.post("alm_integrations/set_pat", params={"almSettings": self.key, "pat": pat, "username": user_name}).ok
+
+    def update(self, **kwargs):
         """Updates a DevOps platform with information from data
 
         :param dict data: data to update the DevOps platform configuration
@@ -120,18 +158,23 @@ class DevopsPlatform(sqobject.SqObject):
         :return: Whether the operation succeeded
         :rtype: bool
         """
-        alm_type = data["type"]
+        alm_type = kwargs["type"]
         if alm_type != self.type:
             util.logger.error("DevOps platform type '%s' for update of %s is incompatible", alm_type, str(self))
             return False
 
-        params = {"key": self.key, "url": data["url"]}
+        params = {"key": self.key, "url": kwargs["url"]}
         if alm_type == "bitbucketcloud":
-            params.update({"clientId": data["clientId"], "workspace": data["workspace"]})
+            params.update({"clientId": kwargs["clientId"], "workspace": kwargs["workspace"]})
         elif alm_type == "github":
-            params.update({"clientId": data["clientId"], "appId": data["appId"]})
+            params.update({"clientId": kwargs["clientId"], "appId": kwargs["appId"]})
 
-        return self.post(f"alm_settings/update_{alm_type}", params=params).ok
+        self.post(f"alm_settings/update_{alm_type}", params=params)
+        self.url = kwargs["url"]
+        for k in ("key", "url"):
+            params.pop(k)
+        self._specific = params
+        return self
 
 
 def count(platf_type=None):
@@ -156,13 +199,10 @@ def get_list(endpoint):
     """
     if endpoint.edition() == "community":
         return _OBJECTS
-    data = json.loads(endpoint.get(_LIST_API).text)
-    for alm_type in _DEVOPS_PLATFORM_TYPES:
+    data = json.loads(endpoint.get(APIS["list"]).text)
+    for alm_type in DEVOPS_PLATFORM_TYPES:
         for alm_data in data.get(alm_type, {}):
-            if alm_data["key"] in _OBJECTS:
-                _OBJECTS[alm_data["key"]].update(alm_data)
-            else:
-                _ = DevopsPlatform(alm_data["key"], endpoint=endpoint, devops_platform_type=alm_type, data=alm_data)
+            DevopsPlatform.load(endpoint, alm_type, alm_data)
     return _OBJECTS
 
 
@@ -175,7 +215,7 @@ def get_object(devops_platform_key, endpoint):
     """
     if len(_OBJECTS) == 0:
         get_list(endpoint)
-    return _OBJECTS.get(devops_platform_key, None)
+    return DevopsPlatform.read(endpoint, devops_platform_key)
 
 
 def exists(devops_platform_key, endpoint):
@@ -200,19 +240,6 @@ def export(endpoint, full=False):
     return json_data
 
 
-def create_or_update_devops_platform(name, data, endpoint):
-    o = _OBJECTS.get(name, None)
-    if o:
-        o.update(data)
-        return o
-    if endpoint.edition() == "developer" and count(data["type"]) >= 1:
-        util.logger.warning("Can't create a 2nd DevOps platform of type '%s' on a developer edition", data["type"])
-        return None
-    else:
-        o = DevopsPlatform(key=name, devops_platform_type=data["type"], endpoint=endpoint, create_data=data)
-    return o
-
-
 def import_config(endpoint, config_data):
     """
     :meta private:
@@ -228,10 +255,14 @@ def import_config(endpoint, config_data):
     if len(_OBJECTS) == 0:
         get_list(endpoint)
     for name, data in devops_settings.items():
-        create_or_update_devops_platform(name=name, data=data, endpoint=endpoint)
+        try:
+            o = DevopsPlatform.read(endpoint, name)
+        except exceptions.ObjectNotFound:
+            o = DevopsPlatform.create(key=name, endpoint=endpoint, plt_type=data["type"])
+        o.update(**data)
 
 
-def platform_type(platform_key, endpoint):
+def devops_type(platform_key, endpoint):
     """
     :return: The type of a DevOps platform (see DEVOPS_PLATFORM_TYPES), or None if not found
     :rtype: str or None
