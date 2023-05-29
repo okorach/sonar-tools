60abb876dbe024dfbbe792685a5014d63a314135
okorach <olivier.korach@gmail.com>
2023-05-29 09:24:35 +0200

    Add env switch to IT

---
+++ test/sonar-env.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+case $1 in
+    lts)
+        export SONAR_HOST_URL=$SONAR_LTS_URL
+        export SONAR_TOKEN=$SONAR_LTS_TOKEN
+        ;;
+    latest)
+        export SONAR_HOST_URL=$SONAR_LATEST_URL
+        export SONAR_TOKEN=$SONAR_LATEST_TOKEN
+        ;;
+    lts-ce)
+        export SONAR_HOST_URL=$SONAR_LTS_CE_URL
+        export SONAR_TOKEN=$SONAR_LTS_CE_TOKEN
+        ;;
+    latest-ce)
+        export SONAR_HOST_URL=$SONAR_LATEST_CE_URL
+        export SONAR_TOKEN=$SONAR_LATEST_CE_TOKEN
+        ;;
+    lts-ee)
+        export SONAR_HOST_URL=$SONAR_LTS_EE_URL
+        export SONAR_TOKEN=$SONAR_LTS_EE_TOKEN
+        ;;
+    latest-ee)
+        export SONAR_HOST_URL=$SONAR_LATEST_EE_URL
+        export SONAR_TOKEN=$SONAR_LATEST_EE_TOKEN
+        ;;
+    -h)
+        echo "Usage: source $0 [lts|latest|lts-ce|latest-ce|lts-ee|latest-ee]"
+        exit 1
+        ;;
+    *)
+        echo "ERROR: environment $1 unknown"
+        echo "Usage: source $0 [lts|latest|lts-ce|latest-ce|lts-ee|latest-ee]"
+        exit 2
+        ;;
+esac    
