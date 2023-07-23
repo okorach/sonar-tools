20478e5e9975dbb97574f332f7549c97b01dd29d
okorach <olivier.korach@gmail.com>
2023-07-23 21:38:50 +0200

    Add user exec

--- test/sonar-env.sh
+++ test/sonar-env.sh
@@ -9,6 +9,14 @@ case $1 in
         export SONAR_HOST_URL=$SONAR_LATEST_URL
         export SONAR_TOKEN=$SONAR_LATEST_TOKEN
         ;;
+    lts-user)
+        export SONAR_HOST_URL=$SONAR_LTS_URL
+        export SONAR_TOKEN=$SONAR_LTS_USER_TOKEN
+        ;;
+    latest-user)
+        export SONAR_HOST_URL=$SONAR_LATEST_URL
+        export SONAR_TOKEN=$SONAR_LATEST_USER_TOKEN
+        ;;
     lts-ce)
         export SONAR_HOST_URL=$SONAR_LTS_CE_URL
         export SONAR_TOKEN=$SONAR_LTS_CE_TOKEN
@@ -26,12 +34,12 @@ case $1 in
         export SONAR_TOKEN=$SONAR_LATEST_EE_TOKEN
         ;;
     -h)
-        echo "Usage: source $0 [lts|latest|lts-ce|latest-ce|lts-ee|latest-ee]"
+        echo "Usage: source $0 [lts|lts-user|latest|latest-user|lts-ce|latest-ce|lts-ee|latest-ee]"
         exit 1
         ;;
     *)
         echo "ERROR: environment $1 unknown"
-        echo "Usage: source $0 [lts|latest|lts-ce|latest-ce|lts-ee|latest-ee]"
+        echo "Usage: source $0 [lts|lts-user|latest|latest-user|lts-ce|latest-ce|lts-ee|latest-ee]"
         exit 2
         ;;
 esac    
