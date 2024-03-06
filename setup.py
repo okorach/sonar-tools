e1086748b146fd13939671aca13dc26b3a513cb4
Olivier Korach <olivier.korach@sonarsource.com>
2024-03-06 17:13:47 +0100

    Add docstring

--- setup.py
+++ setup.py
@@ -17,6 +17,12 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
+
+"""
+
+   Package setup
+
+"""
 import setuptools
 from sonar import version
 
