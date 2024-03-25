a042dc8d5c0c5f40a0a127b4c222e2d60fa56d71
Olivier Korach <olivier.korach@sonarsource.com>
2024-03-25 14:43:25 +0100

    Add sonar-env

--- test/sonar-env.sh
+++ test/sonar-env.sh
@@ -1,53 +1,44 @@
-#!/bin/bash
+#!/usr/bin/bash
+
+source ~/.sonar_rc
 
 case $1 in
     lts)
+        export SONAR_TOKEN=$SONAR_TOKEN_LTS_ADMIN_USER
+        export SONAR_TOKEN_ADMIN=$SONAR_TOKEN_LTS_ADMIN_USER
         export SONAR_HOST_URL=$SONAR_HOST_URL_LTS
-        export SONAR_TOKEN=$SONAR_TOKEN_LTS_U_ADMIN
-        ;;
-    latest)
-        export SONAR_HOST_URL=$SONAR_HOST_URL_LATEST
-        export SONAR_TOKEN=$SONAR_TOKEN_LATEST_U_ADMIN
+        # $SONAR_HOST_URL_LTS
         ;;
     lts-user)
+        export SONAR_TOKEN=$SONAR_TOKEN_LTS_ADMIN_USER
+        export SONAR_TOKEN_ADMIN=$SONAR_TOKEN_LTS_ADMIN_USER
         export SONAR_HOST_URL=$SONAR_HOST_URL_LTS
-        export SONAR_TOKEN=$SONAR_TOKEN_LTS_U_USER
+        # $SONAR_HOST_URL_LTS
         ;;
-    lts-ce-user)
-        export SONAR_HOST_URL=$SONAR_HOST_URL_LTS
-        export SONAR_TOKEN=$SONAR_TOKEN_LTS_U_USER
+    lts-audit)
+        export SONAR_TOKEN=$SONAR_TOKEN_LTS_ADMIN_ANALYSIS
+        export SONAR_TOKEN_ADMIN=$SONAR_TOKEN_LTS_ADMIN_USER
+        export SONAR_HOST_URL=http://localhost:9800
+        ;;
+    latest)
+        export SONAR_TOKEN=$SONAR_TOKEN_LATEST_ADMIN_USER
+        export SONAR_TOKEN_ADMIN=$SONAR_TOKEN_LATEST_ADMIN_USER
+        export SONAR_HOST_URL=$SONAR_HOST_URL_LATEST
         ;;
     latest-user)
+        export SONAR_TOKEN=$SONAR_TOKEN_LATEST_ADMIN_USER
+        export SONAR_TOKEN_ADMIN=$SONAR_TOKEN_LATEST_ADMIN_USER
         export SONAR_HOST_URL=$SONAR_HOST_URL_LATEST
-        export SONAR_TOKEN=$SONAR_TOKEN_LATEST_U_USER
         ;;
-    latest-ce-user)
-        export SONAR_HOST_URL=$SONAR_HOST_URL_LATEST
-        export SONAR_TOKEN=$SONAR_TOKEN_LATEST_U_USER
+    nautilus)
+        export SONAR_TOKEN=$SONAR_TOKEN_NAUTILUS
+        export SONAR_HOST_URL=$SONAR_HOST_URL_NAUTILUS
         ;;
-    lts-ce)
-        export SONAR_HOST_URL=$SONAR_HOST_URL_LTS_CE
-        export SONAR_TOKEN=$SONAR_TOKEN_LTS_CE
-        ;;
-    latest-ce)
-        export SONAR_HOST_URL=$SONAR_HOST_URL_LATEST_CE
-        export SONAR_TOKEN=$SONAR_LATEST_CE_TOKEN
-        ;;
-    lts-ee)
-        export SONAR_HOST_URL=$SONAR_HOST_URL_LTS_EE
-        export SONAR_TOKEN=$SONAR_LTS_EE_TOKEN
-        ;;
-    latest-ee)
-        export SONAR_HOST_URL=$SONAR_HOST_URL_LATEST_EE
-        export SONAR_TOKEN=$SONAR_TOKEN_LATEST_EE
-        ;;
-    -h)
-        echo "Usage: source $0 [lts|lts-user|latest|latest-user|lts-ce|latest-ce|lts-ee|latest-ee]"
-        exit 1
+    sonarcloud)
+        export SONAR_TOKEN=$SONAR_TOKEN_SONARCLOUD
+        export SONAR_HOST_URL=$SONAR_HOST_URL_SONARCLOUD
         ;;
     *)
-        echo "ERROR: environment $1 unknown"
-        echo "Usage: source $0 [lts|lts-user|latest|latest-user|lts-ce|latest-ce|lts-ee|latest-ee]"
-        exit 2
+        echo -u2 "Unsupported sonar environment $1 - Chose between latest lts or nautilus"
         ;;
-esac    
+esac
