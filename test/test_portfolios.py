8a37678c46495ffa21107c4d409f0b2f2054c086
Olivier Korach <olivier.korach@gmail.com>
2024-06-20 12:43:58 +0200

    Fix test_ prefix convention

--- test/test_portfolios.py
+++ test/test_portfolios.py
@@ -31,7 +31,7 @@ EXISTING_PORTFOLIO = "PORT_FAV_PROJECTS"
 TEST_KEY = "MY_PPPPORTFOLIO_KEY"
 
 
-def get_object() -> None:
+def test_get_object() -> None:
     """Test get_object and verify that if requested twice the same object is returned"""
     portf = portfolios.Portfolio.get_object(endpoint=util.SQ, key=EXISTING_PORTFOLIO)
     assert portf.key == EXISTING_PORTFOLIO
@@ -40,20 +40,20 @@ def get_object() -> None:
     assert portf == portf2
 
 
-def get_object_non_existing() -> None:
+def test_get_object_non_existing() -> None:
     """Test exception raised when providing non existing portfolio key"""
     with pytest.raises(exceptions.ObjectNotFound) as e:
         _ = portfolios.Portfolio.get_object(endpoint=util.SQ, key="NON_EXISTING")
     assert str(e.value) == "Portfolio key 'NON_EXISTING' not found"
 
 
-def exists() -> None:
+def test_exists() -> None:
     """Test exist"""
     assert portfolios.exists(endpoint=util.SQ, key="PORT_FAV_PROJECTS")
     assert not portfolios.exists(endpoint=util.SQ, key="NON_EXISTING")
 
 
-def get_list() -> None:
+def test_get_list() -> None:
     """Test portfolio get_list"""
     p_dict = portfolios.get_list(endpoint=util.SQ, key_list="PORT_FAV_PROJECTS,PORTFOLIO_ALL")
     assert "PORT_FAV_PROJECTS" in p_dict
@@ -61,7 +61,7 @@ def get_list() -> None:
     assert len(p_dict) == 2
 
 
-def create_delete() -> None:
+def test_create_delete() -> None:
     """Test portfolio create delete"""
     portfolio = portfolios.Portfolio.create(endpoint=util.SQ, name="MY PPPPPORFOLIO", key=TEST_KEY, description="Creationtest")
     assert portfolio is not None
@@ -72,7 +72,7 @@ def create_delete() -> None:
     assert not portfolios.exists(endpoint=util.SQ, key=TEST_KEY)
 
 
-def add_project() -> None:
+def tes_add_project() -> None:
     """Test addition of a project in manual mode"""
     portfolio = portfolios.Portfolio.create(endpoint=util.SQ, name="A portfolio", key=TEST_KEY, description="Add_project_test")
     project = projects.Project.get_object(endpoint=util.SQ, key="okorach_sonar-tools")
