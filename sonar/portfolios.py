bf3950eaf2d8a7386914cf4dbdb8599ee45a6113
Olivier Korach <olivier.korach@gmail.com>
2024-09-19 13:33:53 +0200

    Change JSON to be more adapted to YAML

--- sonar/portfolios.py
+++ sonar/portfolios.py
@@ -797,3 +797,20 @@ def __create_portfolio_hierarchy(endpoint: pf.Platform, data: types.ApiPayload, 
 def get_api_branch(branch: str) -> str:
     """Returns the value to pass to the API for the branch parameter"""
     return branch if branch != settings.DEFAULT_BRANCH else None
+
+
+def convert_for_yaml(original_json: types.ObjectJsonRepr) -> types.ObjectJsonRepr:
+    """Convert the original JSON defined for JSON export into a JSON format more adapted for YAML export"""
+    new_json = util.dict_to_list(original_json, "key")
+    for p_json in new_json:
+        try:
+            p_json["projects"] = [{"key": k, "branch": br} for k, br in p_json["projects"]["manual"].items()]
+        except KeyError:
+            pass
+        if "portfolios" in p_json:
+            p_json["portfolios"] = convert_for_yaml(p_json["portfolios"])
+        if "applications" in p_json:
+            p_json["applications"] = [{"key": k, "branches": br} for k, br in p_json["applications"].items()]
+        if "permissions" in p_json:
+            p_json["permissions"] = perms.convert_for_yaml(p_json["permissions"])
+    return new_json
