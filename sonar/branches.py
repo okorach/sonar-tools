d4e4f26b554ee1f0c436ba96f62d3f6993c473ce
Olivier Korach <olivier.korach@gmail.com>
2024-09-29 13:06:55 +0200

    Fix circular import

--- sonar/branches.py
+++ sonar/branches.py
@@ -29,7 +29,7 @@ from sonar import platform
 from sonar.util import types
 import sonar.logging as log
 import sonar.sqobject as sq
-from sonar import components, syncer, settings, exceptions
+from sonar import components, settings, exceptions
 from sonar import projects
 import sonar.utilities as util
 
@@ -307,9 +307,11 @@ class Branch(components.Component):
         :return: sync report as tuple, with counts of successful and unsuccessful issue syncs
         :rtype: tuple(report, counters)
         """
+        from sonar.syncer import sync_lists
+
         report, counters = [], {}
         log.info("Syncing %s (%s) and %s (%s) issues", str(self), self.endpoint.url, str(another_branch), another_branch.endpoint.url)
-        (report, counters) = syncer.sync_lists(
+        (report, counters) = sync_lists(
             list(self.get_issues().values()),
             list(another_branch.get_issues().values()),
             self,
@@ -317,7 +319,7 @@ class Branch(components.Component):
             sync_settings=sync_settings,
         )
         log.info("Syncing %s (%s) and %s (%s) hotspots", str(self), self.endpoint.url, str(another_branch), another_branch.endpoint.url)
-        (tmp_report, tmp_counts) = syncer.sync_lists(
+        (tmp_report, tmp_counts) = sync_lists(
             list(self.get_hotspots().values()),
             list(another_branch.get_hotspots().values()),
             self,
