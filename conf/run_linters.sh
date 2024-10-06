1e8c99817ec18bd15c49531b07715d6a5986c8dc
Olivier Korach <olivier.korach@gmail.com>
2024-10-06 11:11:13 +0200

    Set conf config file for linters

--- conf/run_linters.sh
+++ conf/run_linters.sh
@@ -33,7 +33,7 @@ externalIssueReport="$buildDir/external-issue-report.json"
 
 echo "Running pylint"
 rm -f $pylintReport
-pylint --rcfile $CONFDIR/pylintrc ./*.py ./*/*.py -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | tee $pylintReport
+pylint --rcfile $CONFDIR/pylintrc $ROOTDIR/*.py $ROOTDIR/*/*.py -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | tee $pylintReport
 re=$?
 if [ "$re" == "32" ]; then
     >&2 echo "ERROR: pylint execution failed, errcode $re, aborting..."
@@ -43,11 +43,11 @@ fi
 echo "Running flake8"
 rm -f $flake8Report
 # See .flake8 file for settings
-flake8 . >$flake8Report
+flake8 --config "$CONFIG/.flake8" "$ROOTDIR" >$flake8Report
 
 # echo "Running bandit"
 # rm -f $banditReport
 # bandit --exit-zero -f json --skip B311,B303,B101 -r . -x .vscode,./tests >$banditReport
 
 echo "Running shellcheck"
-shellcheck ./*.sh ./*/*.sh -s bash -f json | ./shellcheck2sonar.py >$externalIssueReport
+shellcheck $ROOTDIR/*.sh $ROOTDIR/*/*.sh -s bash -f json | $CONFDIR/shellcheck2sonar.py >$externalIssueReport
