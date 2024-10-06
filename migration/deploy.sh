9ad9b085a4d49d4ed9fba7f8d11b0d6c47492b77
Olivier Korach <olivier.korach@gmail.com>
2024-10-06 11:22:29 +0200

    Build everything from repo root

--- migration/deploy.sh
+++ migration/deploy.sh
@@ -19,6 +19,10 @@
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 
+ME="$( basename "${BASH_SOURCE[0]}" )"
+ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+CONFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 build_image=1
 release=0
 
@@ -40,14 +44,14 @@ while [ $# -ne 0 ]; do
 done
 
 black --line-length=150 .
-rm -rf build dist
-python3 setup_migration.py bdist_wheel
+rm -rf $ROOTDIR/build $ROOTDIR/dist
+python3 $ROOTDIR/setup_migration.py bdist_wheel
 
 # Deploy locally for tests
-pip install --upgrade --force-reinstall dist/sonar_migration-*-py3-*.whl
+pip install --upgrade --force-reinstall $ROOTDIR/dist/sonar_migration-*-py3-*.whl
 
 if [ "$build_image" == "1" ]; then
-    docker build -t olivierkorach/sonar-migration:latest -f migration/snapshot.Dockerfile . --load
+    docker build -t olivierkorach/sonar-migration:latest -f migration/snapshot.Dockerfile $ROOTDIR --load
 fi
 
 # Deploy on pypi.org once released
@@ -55,10 +59,10 @@ if [ "$release" = "1" ]; then
     echo "Confirm release [y/n] ?"
     read -r confirm
     if [ "$confirm" = "y" ]; then
-        python3 -m twine upload dist/sonar_migration-*-py3-*.whl
+        python3 -m twine upload $ROOTDIR/dist/sonar_migration-*-py3-*.whl
     fi
 fi
 
 if [ "$release_docker" = "1" ]; then
-    docker buildx build --push --platform linux/amd64,linux/arm64 -t olivierkorach/sonar-migration:0.3  -t olivierkorach/sonar-migration:latest -f migration/release.Dockerfile .
+    docker buildx build --push --platform linux/amd64,linux/arm64 -t olivierkorach/sonar-migration:0.3  -t olivierkorach/sonar-migration:latest -f migration/release.Dockerfile $ROOTDIR
 fi
