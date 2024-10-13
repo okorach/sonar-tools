bdebdcc9e1b208e016d64573ee624fa01f0bafc2
Olivier Korach <olivier.korach@gmail.com>
2024-10-13 14:57:57 +0200

    Fixes #1407

--- conf/deploy.sh
+++ conf/deploy.sh
@@ -74,4 +74,5 @@ fi
 
 if [ "$release_docker" = "1" ]; then
     docker buildx build --push --platform linux/amd64,linux/arm64 -t olivierkorach/sonar-tools:3.5  -t olivierkorach/sonar-tools:latest -f $ROOTDIR/release.Dockerfile .
+    cd $ROOTDIR && docker pushrm olivierkorach/sonar-tools
 fi
