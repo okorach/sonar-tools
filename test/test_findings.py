367af0ead96359300ec2f6e38925cc00c3ff3940
Olivier Korach <olivier.korach@gmail.com>
2024-10-21 00:50:21 +0200

    Fix JSON test issue impacts replaces issue type on 10.2+

--- test/test_findings.py
+++ test/test_findings.py
@@ -423,9 +423,10 @@ def test_output_format_json() -> None:
             assert k in issue
         if util.SQ.version() >= (10, 2, 0):
             assert "impacts" in issue
+            assert "effort" in issue or "HOTSPOT" in issue["impacts"].get("SECURITY", "")
         else:
             assert "type" in issue
-        assert "effort" in issue or issue.get("type", "") == "SECURITY_HOTSPOT"
+            assert "effort" in issue or issue["type"] == "SECURITY_HOTSPOT"
 
         assert "language" in issue or issue["rule"].startswith("external")
         # Some issues have no author so we cannot expect the below assertion to succeed all the time
