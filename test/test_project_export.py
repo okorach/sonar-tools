b93a3dbe91d293440357a86179b58a21a1d1ed99
Olivier Korach <olivier.korach@gmail.com>
2024-11-18 12:45:07 +0100

    Enhanced tests

--- test/test_project_export.py
+++ test/test_project_export.py
@@ -24,50 +24,56 @@
 
 import os
 import sys
+from collections.abc import Generator
+
 from unittest.mock import patch
 import pytest
 
 import utilities as util
-from sonar import errcodes
+from sonar import errcodes, exceptions
 from cli import projects_cli
 import cli.options as opt
 
 CMD = "projects_cli.py"
-OPTS = [CMD] + util.STD_OPTS + [f"-{opt.EXPORT_SHORT}", f"--{opt.REPORT_FILE}", util.JSON_FILE]
+OPTS = f"{CMD} {util.SQS_OPTS}"
 
 
-def __test_project_export(arguments: list[str], file: str) -> None:
-    """Runs a test command"""
-    util.clean(file)
-    with pytest.raises(SystemExit) as e:
-        with patch.object(sys, "argv", arguments):
-            projects_cli.main()
-    assert int(str(e.value)) == errcodes.OK
-    assert util.file_not_empty(file)
-    util.clean(file)
-
-
-def test_export_all_proj() -> None:
+def test_export_all_proj(get_json_file: Generator[str]) -> None:
     """test_export_all_proj"""
-    __test_project_export(OPTS + [f"--{opt.NBR_THREADS}", "16"], util.JSON_FILE)
+    args = f"{OPTS} --{opt.EXPORT} --{opt.REPORT_FILE} {get_json_file} --{opt.NBR_THREADS} 16"
+    util.run_success_cmd(projects_cli.main, args)
 
 
-def test_export_single_proj() -> None:
+def test_export_single_proj(get_json_file: Generator[str]) -> None:
     """test_export_single_proj"""
-    __test_project_export(OPTS + [f"-{opt.KEYS_SHORT}", "okorach_sonar-tools"], util.JSON_FILE)
+    args = f"{OPTS} --{opt.EXPORT} --{opt.REPORT_FILE} {get_json_file} -{opt.KEYS_SHORT} okorach_sonar-tools"
+    util.run_success_cmd(projects_cli.main, args)
 
 
-def test_export_timeout() -> None:
+def test_export_timeout(get_json_file: Generator[str]) -> None:
     """test_export_timeout"""
-    __test_project_export(OPTS + [f"--{opt.KEYS}", "okorach_sonar-tools", "--exportTimeout", "10"], util.JSON_FILE)
+    cmd = f"{OPTS} --{opt.EXPORT} --{opt.REPORT_FILE} {get_json_file} --{opt.KEYS} okorach_sonar-tools --exportTimeout 10"
+    util.run_success_cmd(projects_cli.main, cmd)
 
 
-def test_export_non_existing_project() -> None:
+def test_export_no_file(get_json_file: Generator[str]) -> None:
+    """test_export_timeout"""
+    cmd = f"{OPTS} --{opt.EXPORT} -{opt.KEYS_SHORT} okorach_sonar-tools"
+    util.run_success_cmd(projects_cli.main, cmd)
+
+
+def test_export_non_existing_project(get_json_file: Generator[str]) -> None:
     """test_config_non_existing_project"""
-    util.clean(util.JSON_FILE)
-    with pytest.raises(SystemExit) as e:
-        with patch.object(sys, "argv", OPTS + [f"-{opt.KEYS_SHORT}", "okorach_sonar-tools,bad_project"]):
-            projects_cli.main()
-    assert int(str(e.value)) == errcodes.NO_SUCH_KEY
-    assert not os.path.isfile(util.JSON_FILE)
-    util.clean(util.JSON_FILE)
+    cmd = f"{OPTS} --{opt.EXPORT} --{opt.REPORT_FILE} {get_json_file} --{opt.KEYS_SHORT} okorach_sonar-tools,bad_project"
+    util.run_failed_cmd(projects_cli.main, cmd, errcodes.NO_SUCH_KEY)
+
+
+def test_export_sonarcloud(get_json_file: Generator[str]) -> None:
+    """test_export_sonarcloud"""
+    cmd = f"{OPTS} --{opt.EXPORT} --{opt.REPORT_FILE} {get_json_file} {util.SC_OPTS}"
+    util.run_failed_cmd(projects_cli.main, cmd, errcodes.UNSUPPORTED_OPERATION)
+
+
+def test_import_no_file() -> None:
+    """test_import_no_file"""
+    util.run_failed_cmd(projects_cli.main, f"{OPTS} --{opt.IMPORT}", errcodes.ARGS_ERROR)
