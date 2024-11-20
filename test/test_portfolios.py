a019310145c62291a06c787868a2b5eaf9c32315
Olivier Korach <olivier.korach@gmail.com>
2024-11-20 09:01:49 +0100

    New portfolios tests

--- test/test_portfolios.py
+++ test/test_portfolios.py
@@ -21,166 +21,262 @@
 
 """ portfolio tests """
 
+from collections.abc import Generator
+import time
 import pytest
 
 import utilities as util
-from sonar import portfolios, projects, exceptions, settings
+from sonar import portfolios, projects, exceptions, settings, logging
 
 EXISTING_PROJECT = "okorach_sonar-tools"
 EXISTING_PORTFOLIO = "PORT_FAV_PROJECTS"
-TEST_KEY = "MY_PPPPORTFOLIO_KEY"
 
 
-def test_get_object() -> None:
+def test_get_object(get_test_portfolio: Generator[portfolios.Portfolio]) -> None:
     """Test get_object and verify that if requested twice the same object is returned"""
     if util.SQ.edition() in ("community", "developer"):
         with pytest.raises(exceptions.UnsupportedOperation):
-            _ = portfolios.Portfolio.get_object(endpoint=util.SQ, key=EXISTING_PORTFOLIO)
-    else:
-        portf = portfolios.Portfolio.get_object(endpoint=util.SQ, key=EXISTING_PORTFOLIO)
-        assert portf.key == EXISTING_PORTFOLIO
-        portf2 = portfolios.Portfolio.get_object(endpoint=util.SQ, key=EXISTING_PORTFOLIO)
-        assert portf2.key == EXISTING_PORTFOLIO
-        assert portf is portf2
+            _ = get_test_portfolio
+        return
+    portf = portfolios.Portfolio.get_object(endpoint=util.SQ, key=EXISTING_PORTFOLIO)
+    assert portf.key == EXISTING_PORTFOLIO
+    portf2 = portfolios.Portfolio.get_object(endpoint=util.SQ, key=EXISTING_PORTFOLIO)
+    assert portf2.key == EXISTING_PORTFOLIO
+    assert portf is portf2
 
 
 def test_get_object_non_existing() -> None:
     """Test exception raised when providing non existing portfolio key"""
     if util.SQ.edition() in ("community", "developer"):
-        with pytest.raises(exceptions.UnsupportedOperation):
-            _ = portfolios.Portfolio.get_object(endpoint=util.SQ, key="NON_EXISTING")
-    else:
-        with pytest.raises(exceptions.ObjectNotFound) as e:
-            _ = portfolios.Portfolio.get_object(endpoint=util.SQ, key="NON_EXISTING")
-        assert str(e.value).endswith("Portfolio key 'NON_EXISTING' not found")
+        return
+
+    with pytest.raises(exceptions.ObjectNotFound) as e:
+        _ = portfolios.Portfolio.get_object(endpoint=util.SQ, key="NON_EXISTING")
+    assert str(e.value).endswith("Portfolio key 'NON_EXISTING' not found")
 
 
 def test_exists() -> None:
     """Test exist"""
     if util.SQ.edition() in ("community", "developer"):
-        with pytest.raises(exceptions.UnsupportedOperation):
-            _ = portfolios.exists(endpoint=util.SQ, key="PORT_FAV_PROJECTS")
-    else:
-        assert portfolios.exists(endpoint=util.SQ, key="PORT_FAV_PROJECTS")
-        assert not portfolios.exists(endpoint=util.SQ, key="NON_EXISTING")
+        return
+    assert portfolios.exists(endpoint=util.SQ, key="PORT_FAV_PROJECTS")
+    assert not portfolios.exists(endpoint=util.SQ, key="NON_EXISTING")
 
 
 def test_get_list() -> None:
     """Test portfolio get_list"""
     k_list = ["PORT_FAV_PROJECTS", "PORTFOLIO_ALL"]
     if util.SQ.edition() in ("community", "developer"):
-        with pytest.raises(exceptions.UnsupportedOperation):
-            _ = portfolios.get_list(endpoint=util.SQ, key_list=k_list)
-    else:
-        p_dict = portfolios.get_list(endpoint=util.SQ, key_list=k_list)
-        assert sorted(k_list) == sorted(list(p_dict.keys()))
+        return
+
+    p_dict = portfolios.get_list(endpoint=util.SQ, key_list=k_list)
+    assert sorted(k_list) == sorted(list(p_dict.keys()))
 
 
-def test_create_delete() -> None:
+def test_create_delete(get_test_portfolio: Generator[portfolios.Portfolio]) -> None:
     """Test portfolio create delete"""
     if util.SQ.edition() in ("community", "developer"):
-        with pytest.raises(exceptions.UnsupportedOperation):
-            _ = portfolios.Portfolio.create(endpoint=util.SQ, name="MY PPPPPORFOLIO", key=TEST_KEY, description="Creationtest")
-    else:
-        portfolios.delete(endpoint=util.SQ, key=TEST_KEY)
-        portfolio = portfolios.Portfolio.create(endpoint=util.SQ, name="MY PPPPPORFOLIO", key=TEST_KEY, description="Creationtest")
-        assert portfolio is not None
-        assert portfolio.key == TEST_KEY
-        assert "none" in portfolio.selection_mode()
-        assert portfolio.name == "MY PPPPPORFOLIO"
-        portfolio.delete()
-        assert not portfolios.exists(endpoint=util.SQ, key=TEST_KEY)
+        return
+    portfolio = get_test_portfolio
+    assert portfolio is not None
+    assert portfolio.key == util.TEMP_KEY
+    assert "none" in portfolio.selection_mode()
+    assert portfolio.name == util.TEMP_KEY
+    assert portfolio.is_toplevel()
+    with pytest.raises(exceptions.ObjectAlreadyExists):
+        portfolios.Portfolio.create(endpoint=util.SQ, key=util.TEMP_KEY)
+    portfolio.delete()
+    assert not portfolios.exists(endpoint=util.SQ, key=util.TEMP_KEY)
 
 
-def test_add_project() -> None:
+def test_add_project(get_test_portfolio: Generator[portfolios.Portfolio]) -> None:
     """Test addition of a project in manual mode"""
     if util.SQ.edition() in ("community", "developer"):
-        with pytest.raises(exceptions.UnsupportedOperation):
-            _ = portfolios.Portfolio.create(endpoint=util.SQ, name="A portfolio", key=TEST_KEY, description="Add_project_test")
-    else:
-        portfolios.delete(endpoint=util.SQ, key=TEST_KEY)
-        p = portfolios.Portfolio.create(endpoint=util.SQ, name="A portfolio", key=TEST_KEY, description="Add_project_test")
-        assert "none" in p.selection_mode()
+        return
+    p = get_test_portfolio
+    assert "none" in p.selection_mode()
 
-        project = projects.Project.get_object(endpoint=util.SQ, key="okorach_sonar-tools")
-        assert "none" in p.selection_mode()
-        p.add_projects({project.key})
-        mode = p.selection_mode()
-        assert "manual" in mode
-        assert mode["manual"] == {"okorach_sonar-tools": {settings.DEFAULT_BRANCH}}
-        assert p.projects() == {"okorach_sonar-tools": {settings.DEFAULT_BRANCH}}
-        p.delete()
-        assert not portfolios.exists(endpoint=util.SQ, key=TEST_KEY)
+    project = projects.Project.get_object(endpoint=util.SQ, key=util.LIVE_PROJECT)
+    assert "none" in p.selection_mode()
+    p._selection_mode = None
+    p.selection_mode()
+    assert not p.has_project(project.key)
+    p.add_projects({project.key})
+    mode = p.selection_mode()
+    assert "manual" in mode
+    assert mode["manual"] == {util.LIVE_PROJECT: {settings.DEFAULT_BRANCH}}
+    assert p.projects() == {util.LIVE_PROJECT: {settings.DEFAULT_BRANCH}}
+    components = p.get_components()
+    # assert len(components) == 1
+    # assert list(components.keys()) == [util.LIVE_PROJECT]
+    assert p.has_project(project.key)
+
+    p.add_project_branches(project.key, [settings.DEFAULT_BRANCH, "develop"])
+    p.add_project_branches(project.key, ["comma,branch", "develop"])
+    assert p.recompute()
 
 
-def test_tags_mode() -> None:
+def test_tags_mode(get_test_portfolio: Generator[portfolios.Portfolio]) -> None:
     """Test tag mode"""
     if util.SQ.edition() in ("community", "developer"):
         with pytest.raises(exceptions.UnsupportedOperation):
-            _ = portfolios.Portfolio.create(endpoint=util.SQ, name="A portfolio", key=TEST_KEY, description="Add_project_test")
-    else:
-        portfolios.delete(endpoint=util.SQ, key=TEST_KEY)
-        p = portfolios.Portfolio.create(endpoint=util.SQ, name="A portfolio", key=TEST_KEY, description="Add_project_test")
-        in_tags = ["foss", "favorites"]
-        p.set_tags_mode(in_tags)
-        mode = p.selection_mode()
-        assert "tags" in mode and mode["tags"].sort() == in_tags.sort()
-        assert mode["branch"] == settings.DEFAULT_BRANCH
+            _ = get_test_portfolio
+        return
+    p = get_test_portfolio
+    in_tags = ["foss", "favorites"]
+    p.set_tags_mode(in_tags)
+    mode = p.selection_mode()
+    assert "tags" in mode and mode["tags"].sort() == in_tags.sort()
+    assert mode["branch"] == settings.DEFAULT_BRANCH
 
-        p.set_tags_mode(tags=in_tags, branch="some_branch")
-        mode = p.selection_mode()
-        assert "tags" in mode and mode["tags"].sort() == in_tags.sort()
-        assert mode["branch"] == "some_branch"
-        p.delete()
-        assert not portfolios.exists(endpoint=util.SQ, key=TEST_KEY)
+    p.set_tags_mode(tags=in_tags, branch="some_branch")
+    mode = p.selection_mode()
+    assert "tags" in mode and mode["tags"].sort() == in_tags.sort()
+    assert mode["branch"] == "some_branch"
+    assert p.recompute()
 
 
-def test_regexp_mode() -> None:
+def test_regexp_mode(get_test_portfolio: Generator[portfolios.Portfolio]) -> None:
     """Test regexp mode"""
     if util.SQ.edition() in ("community", "developer"):
-        with pytest.raises(exceptions.UnsupportedOperation):
-            _ = portfolios.Portfolio.create(endpoint=util.SQ, name="A portfolio", key=TEST_KEY, description="Add_project_test")
-    else:
-        portfolios.delete(endpoint=util.SQ, key=TEST_KEY)
-        p = portfolios.Portfolio.create(endpoint=util.SQ, name="A portfolio", key=TEST_KEY, description="Add_project_test")
-        in_regexp = "^FAVORITES.*$"
-        p.set_regexp_mode(in_regexp)
-        mode = p.selection_mode()
-        assert "regexp" in mode and mode["regexp"] == in_regexp
-        assert "branch" in mode and mode["branch"] == settings.DEFAULT_BRANCH
+        return
+    p = get_test_portfolio
+    in_regexp = "^FAVORITES.*$"
+    p.set_regexp_mode(in_regexp)
+    mode = p.selection_mode()
+    assert "regexp" in mode and mode["regexp"] == in_regexp
+    assert "branch" in mode and mode["branch"] == settings.DEFAULT_BRANCH
+    assert p.recompute()
 
-        in_regexp = "^BRANCH_FAVORITES.*$"
-        p.set_regexp_mode(regexp=in_regexp, branch="develop")
-        mode = p.selection_mode()
-        assert "regexp" in mode and mode["regexp"] == in_regexp
-        assert "branch" in mode and mode["branch"] == "develop"
-        p.delete()
-        assert not portfolios.exists(endpoint=util.SQ, key=TEST_KEY)
+    in_regexp = "^BRANCH_FAVORITES.*$"
+    p.set_regexp_mode(regexp=in_regexp, branch="develop")
+    mode = p.selection_mode()
+    assert "regexp" in mode and mode["regexp"] == in_regexp
+    assert "branch" in mode and mode["branch"] == "develop"
+    assert p.recompute()
 
 
-def test_permissions_1() -> None:
+def test_remaining_projects_mode(get_test_portfolio: Generator[portfolios.Portfolio]) -> None:
+    """Test regexp mode"""
+    if util.SQ.edition() in ("community", "developer"):
+        return
+    p = get_test_portfolio
+    p.set_remaining_projects_mode()
+    assert p._selection_mode == {"rest": True, "branch": settings.DEFAULT_BRANCH}
+    p.set_remaining_projects_mode("develop")
+    assert p._selection_mode == {"rest": True, "branch": "develop"}
+
+
+def test_none_mode(get_test_portfolio: Generator[portfolios.Portfolio]) -> None:
+    """Test regexp mode"""
+    if util.SQ.edition() in ("community", "developer"):
+        return
+    p = get_test_portfolio
+    p.set_remaining_projects_mode()
+    assert p._selection_mode == {"rest": True, "branch": settings.DEFAULT_BRANCH}
+    p.set_none_mode()
+    assert p._selection_mode == {}
+
+
+def test_attributes(get_test_portfolio: Generator[portfolios.Portfolio]) -> None:
+    """Test regexp mode"""
+    if util.SQ.edition() in ("community", "developer"):
+        return
+    p = get_test_portfolio
+    new_name = "foobar"
+    p.set_name(new_name)
+    p.refresh()
+    assert p.name == new_name
+    p.recompute()  # New name is not update in search if portfolio is not recomputed
+    time.sleep(3)
+    data = portfolios.search_by_name(util.SQ, new_name)
+    assert data["key"] == p.key
+    p.set_description("some description of a portfolio")
+    p.refresh()
+    assert p._description == "some description of a portfolio"
+
+
+def test_permissions_1(get_test_portfolio: Generator[portfolios.Portfolio]) -> None:
     """Test permissions"""
     if util.SQ.edition() in ("community", "developer"):
-        with pytest.raises(exceptions.UnsupportedOperation):
-            _ = portfolios.Portfolio.create(endpoint=util.SQ, name="A portfolio", key=TEST_KEY, description="Add_project_test")
-    else:
-        portfolios.delete(endpoint=util.SQ, key=TEST_KEY)
-        p = portfolios.Portfolio.create(endpoint=util.SQ, name="A portfolio", key=TEST_KEY, description="Add_project_test")
-        p.set_permissions({"groups": {"sonar-users": ["user", "admin"], "sonar-administrators": ["user", "admin"]}})
-        # assert p.permissions().to_json()["groups"] == {"sonar-users": ["user", "admin"], "sonar-administrators": ["user", "admin"]}
-        p.delete()
-        assert not portfolios.exists(endpoint=util.SQ, key=TEST_KEY)
+        return
+    p = get_test_portfolio
+    p.set_permissions({"groups": {"sonar-users": ["user", "admin"], "sonar-administrators": ["user", "admin"]}})
+    # assert p.permissions().to_json()["groups"] == {"sonar-users": ["user", "admin"], "sonar-administrators": ["user", "admin"]}
 
 
-def test_permissions_2() -> None:
+def test_permissions_2(get_test_portfolio: Generator[portfolios.Portfolio]) -> None:
     """Test permissions"""
     if util.SQ.edition() in ("community", "developer"):
-        with pytest.raises(exceptions.UnsupportedOperation):
-            _ = portfolios.Portfolio.create(endpoint=util.SQ, name="A portfolio", key=TEST_KEY, description="Add_project_test", visibility="private")
-    else:
-        portfolios.delete(endpoint=util.SQ, key=TEST_KEY)
-        p = portfolios.Portfolio.create(endpoint=util.SQ, name="A portfolio", key=TEST_KEY, description="Add_project_test", visibility="private")
-        p.set_permissions({"groups": {"sonar-users": ["user"], "sonar-administrators": ["user", "admin"]}})
-        # assert p.permissions().to_json()["groups"] == {"sonar-users": ["user"], "sonar-administrators": ["user", "admin"]}
-        p.delete()
-        assert not portfolios.exists(endpoint=util.SQ, key=TEST_KEY)
+        return
+    p = get_test_portfolio
+    p.set_permissions({"groups": {"sonar-users": ["user"], "sonar-administrators": ["user", "admin"]}})
+    # assert p.permissions().to_json()["groups"] == {"sonar-users": ["user"], "sonar-administrators": ["user", "admin"]}
+
+
+def test_audit(get_test_portfolio: Generator[portfolios.Portfolio]) -> None:
+    """test_audit"""
+    if util.SQ.edition() in ("community", "developer"):
+        return
+    p = get_test_portfolio
+    settings = {}
+    assert len(p.audit(settings)) > 0
+    settings["audit.portfolios.empty"] = False
+    settings["audit.portfolios.singleton"] = False
+    p.audit(settings)
+    settings["audit.portfolios"] = False
+    assert len(portfolios.audit(util.SQ, settings)) == 0
+
+
+def test_add_standard_subp(get_test_subportfolio: Generator[portfolios.Portfolio]) -> None:
+    """test_standard_subp"""
+    if util.SQ.edition() in ("community", "developer"):
+        return
+    subp = get_test_subportfolio
+    assert subp.parent.key == util.TEMP_KEY
+    parent = portfolios.Portfolio.get_object(util.SQ, key=util.TEMP_KEY)
+    subp_d = parent.sub_portfolios()
+    assert len(subp_d) == 1
+    assert list(subp_d.keys()) == [util.TEMP_KEY_3]
+    assert list(subp_d.values())[0] == subp
+
+
+def test_add_standard_subp_2(get_test_portfolio: Generator[portfolios.Portfolio]) -> None:
+    """test_add_standard_subp_2"""
+    util.start_logging()
+    if util.SQ.edition() in ("community", "developer"):
+        return
+    parent = get_test_portfolio
+    subp = parent.add_subportfolio(key=util.TEMP_KEY_3)
+    subp_d = parent.sub_portfolios()
+    assert len(subp_d) == 1
+    assert list(subp_d.keys()) == [util.TEMP_KEY_3]
+    assert list(subp_d.values())[0] == subp
+    # subp.refresh()
+    logging.debug("%s is toplevel = %s", str(subp), str(subp.is_toplevel()))
+    assert not subp.is_toplevel()
+    assert parent.is_parent_of(subp.key)
+    assert subp.is_subporfolio_of(parent.key)
+
+
+def test_add_ref_subp(get_test_portfolio: Generator[portfolios.Portfolio], get_test_portfolio_2: Generator[portfolios.Portfolio]) -> None:
+    """test_add_standard_subp_2"""
+    if util.SQ.edition() in ("community", "developer"):
+        return
+    parent = get_test_portfolio
+    ref = get_test_portfolio_2
+    subp = parent.add_subportfolio(key=ref.key, by_ref=True)
+    subp_d = parent.sub_portfolios()
+    assert len(subp_d) == 1
+    assert list(subp_d.keys()) == [ref.key]
+    assert list(subp_d.values())[0] == subp
+
+
+def test_export() -> None:
+    """test_export"""
+    if util.SQ.edition() in ("community", "developer"):
+        return
+    json_exp = portfolios.export(util.SQ, {})
+    yaml_exp = portfolios.convert_for_yaml(json_exp)
+    assert isinstance(yaml_exp, list)
+    assert len(yaml_exp) == len(json_exp)
