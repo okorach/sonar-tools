fdd4a26b99a3f2c0352e9b3e681a1e8af4e0e10a
Olivier Korach <olivier.korach@gmail.com>
2024-11-23 18:22:24 +0100

    Docker integration tests

---
+++ test/it-docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# sonar-tools
+# Copyright (C) 2021-2024 Olivier Korach
+# mailto:olivier.korach AT gmail DOT com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+env=${1:-gen}
+
+DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+source "$DIR/test-tools.sh"
+
+DOCKER_COMMON="docker run --rm -w `pwd` -v `pwd`:/home/sonar olivierkorach/sonar-tools"
+CREDS="-u $SONAR_HOST_URL -t $SONAR_TOKEN"
+
+for cmd in loc measures-export findings-export config audit housekeeper rules
+do
+    f="docker-$env-$cmd.csv"; run_test "$f" $DOCKER_COMMON "sonar-$cmd" $CREDS
+    mv "$f" "$TMP"
+done
