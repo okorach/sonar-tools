145da9d7d11847a4874054a99398b5f571ef198d
Olivier Korach <olivier.korach@gmail.com>
2024-11-27 21:30:16 +0100

    Make IT compatible with docker

--- test/it-docker.sh
+++ test/it-docker.sh
@@ -23,6 +23,7 @@ env=${1:-gen}
 
 DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source "$DIR/it-tools.sh"
+IT_LOG_FILE="$TMP/it.log"
 
 # DOCKER_COMMON="docker run --rm -w `pwd` -v `pwd`:/home/sonar olivierkorach/sonar-tools"
 DOCKER_COMMON="docker run --rm olivierkorach/sonar-tools"
@@ -45,16 +46,23 @@ f="$root-housekeeper-stdout.csv"; run_test_stdout "$f" $DOCKER_COMMON "sonar-hou
 f="$root-tools.txt"; run_test_stdout "$f" $DOCKER_COMMON
 
 DOCKER_COMMON="docker run -w /home/sonar -v `pwd`:/home/sonar --rm olivierkorach/sonar-tools"
+HOST_IT_LOG=$IT_LOG_FILE
+IT_LOG_FILE="it.log"
+CREDS="-u $SONAR_HOST_URL -t $SONAR_TOKEN -l $IT_LOG_FILE"
 
 for cmd in loc measures-export rules
 do
     f="$root-$cmd-file.csv"; run_test "$f" $DOCKER_COMMON "sonar-$cmd" $CREDS
     mv "$f" "$TMP"
+    cat $IT_LOG_FILE >>$HOST_IT_LOG
 done
 
 f="$root-audit-file.csv"; run_test "$f" $DOCKER_COMMON sonar-audit -w settings,users,groups,qualitygates $CREDS
 mv "$f" "$TMP"
+cat $IT_LOG_FILE >>$HOST_IT_LOG
 f="$root-findings-file.csv"; run_test "$f" $DOCKER_COMMON sonar-findings-export --severities BLOCKER,CRITICAL $CREDS
 mv "$f" "$TMP"
+cat $IT_LOG_FILE >>$HOST_IT_LOG
 f="$root-config-file.json"; run_test "$f" $DOCKER_COMMON sonar-config -e -w qualitygates,users,groups $CREDS
 mv "$f" "$TMP"
+cat $IT_LOG_FILE >>$HOST_IT_LOG
--- test/it.sh
+++ test/it.sh
@@ -101,6 +101,8 @@ do
         export SONAR_HOST_URL="http://localhost:$sqport"
     fi
 
+    logmsg "=====> IT $env sonar-measures-export"
+
     f="measures-$env-unrel.csv"; run_test "$f" sonar-measures-export -b -m _main --withURL
     f="measures-$env-2.csv";     run_test_stdout "$f" sonar-measures-export -b -m _main --withURL
     f="measures-$env-3.csv";     run_test_stdout "$f" sonar-measures-export -b -p -r -d -m _all
@@ -113,7 +115,7 @@ do
     f="measures-history-$env-2.csv";     run_test "$f" sonar-measures-export -b -k okorach_sonar-tools --history --asTable
     f="measures-history-$env-3.json";    run_test "$f" sonar-measures-export -b --history
 
-    logmsg "IT $env sonar-findings-export"
+    logmsg "=====> IT $env sonar-findings-export"
 
     f="findings-$env-unrel.csv";  run_test "$f" sonar-findings-export -v DEBUG
     f="findings-$env-1.json";     run_test "$f" sonar-findings-export
@@ -125,32 +127,33 @@ do
         logmsg "IT $env sonar-audit SKIPPED"
         logmsg "IT $env sonar-housekeeper SKIPPED"
     else
-        logmsg "IT $env sonar-audit"
+        logmsg "=====> IT $env sonar-audit"
         f="audit-$env-unrel.csv";     run_test_stdout "$f" sonar-audit
         f="audit-$env-1.json";        run_test "$f" sonar-audit
         f="audit-$env-2.json";        run_test_stdout "$f" sonar-audit --format json --what qualitygates,qualityprofiles,settings
         f="audit-$env-3.csv";         run_test_stdout "$f" sonar-audit  --csvSeparator '+' --format csv
 
-        logmsg "IT $env sonar-housekeeper"
+        logmsg "=====> IT $env sonar-housekeeper"
         f="housekeeper-$env-1.csv";   run_test_stdout "$f" sonar-housekeeper -P 365 -B 90 -T 180 -R 30
     fi
 
-    logmsg "IT $env sonar-loc"
+    logmsg "=====> IT $env sonar-loc"
     f="loc-$env-1.csv";           run_test_stdout "$f" sonar-loc
     f="loc-$env-unrel.csv";       run_test_stdout "$f" sonar-loc -n -a
     f="loc-$env-2.csv";           run_test "$f" sonar-loc -n -a --csvSeparator ';'
 
-    logmsg "sonar-rules $env"
+    logmsg "=====> IT sonar-rules $env"
     f="rules-$env-1.csv";         run_test_stdout "$f" sonar-rules -e
     f="rules-$env-2.csv";         run_test "$f" sonar-rules -e
     f="rules-$env-3.json";        run_test_stdout "$f" sonar-rules -e --format json
     f="rules-$env-4.json";        run_test "$f" sonar-rules -e
 
-    logmsg "sonar-config $env"
+    logmsg "=====> IT sonar-config $env"
     f="config-$env-1.json";       run_test_stdout "$f" sonar-config -e -w "qualitygates, qualityprofiles, projects" -k okorach_audio-video-tools,okorach_sonar-tools
     f="config-$env-2.json";       run_test_stdout "$f" sonar-config --export
     f="config-$env-unrel.json";   run_test "$f" sonar-config --export
 
+    logmsg "=====> IT sonar-projects-export $env"
     if [ $noExport -eq 1 ]; then
         logmsg "sonar-projects-export $env test skipped"
     elif [ "$env" = "sonarcloud" ]; then
@@ -160,25 +163,26 @@ do
         f="proj-export-$env-2.json"; run_test "$f" sonar-projects-export
     fi
 
-    logmsg "sonar-findings-export $env ADMIN export"
+    logmsg "=====> sonar-findings-export $env ADMIN export"
     f1="findings-$env-admin.csv";   run_test "$f1" sonar-findings-export -v DEBUG -k okorach_audio-video-tools,okorach_sonar-tools
+
+    #--------------------------------------------------------------------------
+    source "$DIR"/it-docker.sh "$env"
 
     if [ "$env" = "sonarcloud" ]; then
         logmsg "sonar-projects-export $env SKIPPED"
     else
-        logmsg "sonar-findings-export $env USER export"
+        logmsg "=====> sonar-findings-export $env USER export"
         export SONAR_TOKEN=$SONAR_TOKEN_USER_USER
         f2="findings-$env-user.csv";    run_test "$f2" sonar-findings-export -v DEBUG -k okorach_audio-video-tools,okorach_sonar-tools
     fi
-
-    source "$DIR"/it-docker.sh "$env"
 
     # Restore admin token as long as previous version is 2.9 or less
     logmsg "Restore sonar-tools last released version"
     pip install --force-reinstall sonar-tools 1>$IT_LOG_FILE 2>&1; 
     
     export SONAR_TOKEN="$SONAR_TOKEN_ADMIN_USER"
-    logmsg "IT released tools $env"
+    logmsg "=====> IT released tools $env"
     f="measures-$env-rel.csv"; run_test "$f" sonar-measures-export -b -m _main --withURL
     f="findings-$env-rel.csv"; run_test "$f" sonar-findings-export
     # Breaks in version 3.3
@@ -201,7 +205,7 @@ do
         announce_test "$f-$env diff"
         test_passed_if_identical "$root-rel.json" "$root-unrel.json"
     done
-    announce_test "findings-$env admin vs user diff"
+    announce_test "=====> findings-$env admin vs user diff"
     test_passed_if_identical "$TMP/findings-$env-admin.csv" "$TMP/findings-$env-user.csv"
 
     if [ "$env" != "sonarcloud" ]; then
