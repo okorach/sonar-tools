5a0dfd2181f60a853142d322ef6786fc968f5a30
Olivier Korach <olivier.korach@gmail.com>
2024-12-22 14:49:52 +0100

    DELETE API

--- sonar/projects.py
+++ sonar/projects.py
@@ -128,7 +128,7 @@ class Project(components.Component):
     CACHE = cache.Cache()
     SEARCH_KEY_FIELD = "key"
     SEARCH_RETURN_FIELD = "components"
-    API = {c.CREATE: "projects/create", c.SEARCH: "projects/search", "SET_TAGS": "project_tags/set", "GET_TAGS": "components/show"}
+    API = {c.CREATE: "projects/create", c.DELETE: "projects/delete", c.SEARCH: "projects/search", "SET_TAGS": "project_tags/set", "GET_TAGS": "components/show"}
     # SEARCH_API = "components/search_projects" - This one does not require admin permission but returns APPs too
 
     def __init__(self, endpoint: pf.Platform, key: str) -> None:
@@ -357,7 +357,7 @@ class Project(components.Component):
         """
         loc = int(self.get_measure("ncloc", fallback="0"))
         log.info("Deleting %s, name '%s' with %d LoCs", str(self), self.name, loc)
-        ok = sqobject.delete_object(self, "projects/delete", {"project": self.key}, Project.CACHE)
+        ok = sqobject.delete_object(self, Project.API[c.DELETE], {"project": self.key}, Project.CACHE)
         log.info("Successfully deleted %s - %d LoCs", str(self), loc)
         return ok
 
