2d32de0404d38ef06133590cdb2f97e6d399a054
Olivier Korach <olivier.korach@gmail.com>
2025-01-02 14:07:57 +0100

    Add credentials per target platform

---
+++ test/credentials-lts.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#
+# sonar-tools tests
+# Copyright (C) 2025 Olivier Korach
+# mailto:olivier.korach AT gmail DOT com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+from os import getenv
+
+TARGET_PLATFORM = "http://localhost:9000"
+TARGET_TOKEN = getenv("SONAR_TOKEN_LTS_ADMIN_USER")
