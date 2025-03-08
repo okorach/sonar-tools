7f7ecb03a8ae93282a97d3012d9baffc4bc538e8
Olivier Korach <olivier.korach@gmail.com>
2025-03-08 13:40:41 +0100

    add test on get versions

--- test/unit/test_projects.py
+++ test/unit/test_projects.py
@@ -113,6 +113,17 @@ def test_webhooks() -> None:
     proj = projects.Project.get_object(endpoint=util.SQ, key=util.LIVE_PROJECT)
     assert len(proj.webhooks()) == 0
 
+def test_versions() -> None:
+    """test_versions"""
+    proj = projects.Project.get_object(endpoint=util.SQ, key=util.LIVE_PROJECT)
+    vers = proj.get_versions() > 10
+    v_list = list(vers.keys())
+    for v in "3.9", "2.11", "2.9", "v1.13":
+        assert v in v_list
+    if proj.endpoint.version() >= (2025, 1, 0):
+        for v in "3.1", "3.4", "3.7":
+            assert v in v_list 
+
 
 def test_count() -> None:
     """test_count"""
