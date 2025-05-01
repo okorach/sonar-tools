eb3d7c000f9fc34d4e964e38e8cd0be9f76a83bd
Olivier Korach <olivier.korach@gmail.com>
2025-05-01 14:40:15 +0200

    Fix typo

--- test/unit/test_findings_sync.py
+++ test/unit/test_findings_sync.py
@@ -36,7 +36,7 @@ import cli.options as opt
 
 
 CMD = "sonar-findings-sync.py"
-PLAT_OPTS = f"--{opt.URL} {os.getenv('SONAR_HOST_URL')} --{opt.TOKEN} {s.getenv('SONAR_TOKEN_ADMIN_USER')} -U {os.getenv('SONAR_HOST_URL_TEST')} -T {os.getenv('SONAR_TOKEN_SYNC_USER')}"
+PLAT_OPTS = f"--{opt.URL} {os.getenv('SONAR_HOST_URL')} --{opt.TOKEN} {os.getenv('SONAR_TOKEN_ADMIN_USER')} -U {os.getenv('SONAR_HOST_URL_TEST')} -T {os.getenv('SONAR_TOKEN_SYNC_USER')}"
 SYNC_OPTS = f"--login syncer -{opt.KEYS_SHORT} {util.LIVE_PROJECT} -K TESTSYNC -b master -B main"
 
 
