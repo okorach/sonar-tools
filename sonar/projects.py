ed15812849894beeb915fec22f758c3585ee9f1f
Olivier Korach <olivier.korach@sonarsource.com>
2025-06-06 08:47:47 +0200

    Raise object not found

--- sonar/projects.py
+++ sonar/projects.py
@@ -762,6 +762,8 @@ class Project(components.Component):
         except RequestException as e:
             if "Dump file does not exist" in util.sonar_error(e.response):
                 return f"FAILED/{tasks.ZIP_MISSING}"
+            if e.response.status_code == HTTPStatus.NOT_FOUND:
+                raise exceptions.ObjectNotFound(self.key, f"Project key '{self.key}' not found")
             util.handle_error(e, f"importing zip of {str(self)} {mode}", catch_all=True)
             return f"FAILED/{util.http_error_string(e.response.status_code)}"
         except ConnectionError as e:
