ebbf5d1dc87eec586e67d806a53ba83db638ce53
Olivier Korach <olivier.korach@sonarsource.com>
2025-06-12 13:33:45 +0200

    Reduce cog complexity

--- test/unit/test_loc.py
+++ test/unit/test_loc.py
@@ -104,18 +104,19 @@ def test_loc_proj_all_options() -> None:
     cmd = " ".join(CSV_OPTS + ALL_OPTIONS)
     if util.SQ.edition() == c.CE:
         util.run_failed_cmd(loc.main, cmd, errcodes.UNSUPPORTED_OPERATION)
-    else:
-        util.run_success_cmd(loc.main, cmd, post_cleanup=False)
-        # Check file contents
-        with open(file=util.CSV_FILE, mode="r", encoding="utf-8") as fh:
-            reader = csv.reader(fh)
-            row = next(reader)
-            for k in "# project key", "branch", "ncloc", "project name", "last analysis", "URL":
-                assert k in row
-            for line in reader:
-                assert util.is_url(line[5])
-                assert line[4] == "" or util.is_datetime(line[4])
-                assert util.is_integer(line[2])
+        return
+
+    util.run_success_cmd(loc.main, cmd, post_cleanup=False)
+    # Check file contents
+    with open(file=util.CSV_FILE, mode="r", encoding="utf-8") as fh:
+        reader = csv.reader(fh)
+        row = next(reader)
+        for k in "# project key", "branch", "ncloc", "project name", "last analysis", "URL":
+            assert k in row
+        for line in reader:
+            assert util.is_url(line[5])
+            assert line[4] == "" or util.is_datetime(line[4])
+            assert util.is_integer(line[2])
     util.clean(util.CSV_FILE)
 
 
@@ -124,18 +125,19 @@ def test_loc_apps_all_options() -> None:
     cmd = " ".join(CSV_OPTS + ["--apps"] + ALL_OPTIONS)
     if util.SQ.edition() == c.CE:
         util.run_failed_cmd(loc.main, cmd, errcodes.UNSUPPORTED_OPERATION)
-    else:
-        util.run_success_cmd(loc.main, cmd, post_cleanup=False)
-        # Check file contents
-        with open(file=util.CSV_FILE, mode="r", encoding="utf-8") as fh:
-            reader = csv.reader(fh)
-            row = next(reader)
-            for k in "# app key", "branch", "ncloc", "app name", "last analysis", "URL":
-                assert k in row
-            for line in reader:
-                assert util.is_url(line[5])
-                assert line[4] == "" or util.is_datetime(line[4])
-                assert util.is_integer(line[2])
+        return
+
+    util.run_success_cmd(loc.main, cmd, post_cleanup=False)
+    # Check file contents
+    with open(file=util.CSV_FILE, mode="r", encoding="utf-8") as fh:
+        reader = csv.reader(fh)
+        row = next(reader)
+        for k in "# app key", "branch", "ncloc", "app name", "last analysis", "URL":
+            assert k in row
+        for line in reader:
+            assert util.is_url(line[5])
+            assert line[4] == "" or util.is_datetime(line[4])
+            assert util.is_integer(line[2])
     util.clean(util.CSV_FILE)
 
 
@@ -144,18 +146,18 @@ def test_loc_portfolios_all_options() -> None:
     cmd = " ".join(CSV_OPTS + ["--portfolios"] + ALL_OPTIONS)
     if util.SQ.edition() in (c.CE, c.DE):
         util.run_failed_cmd(loc.main, cmd, errcodes.UNSUPPORTED_OPERATION)
-    else:
-        util.run_success_cmd(loc.main, cmd, post_cleanup=False)
-        # Check file contents
-        with open(file=util.CSV_FILE, mode="r", encoding="utf-8") as fh:
-            reader = csv.reader(fh)
-            row = next(reader)
-            for k in "# portfolio key", "ncloc", "portfolio name", "last analysis", "URL":
-                assert k in row
-            for line in reader:
-                assert util.is_url(line[4])
-                assert line[3] == "" or util.is_datetime(line[3])
-                assert util.is_integer(line[1])
+        return
+    util.run_success_cmd(loc.main, cmd, post_cleanup=False)
+    # Check file contents
+    with open(file=util.CSV_FILE, mode="r", encoding="utf-8") as fh:
+        reader = csv.reader(fh)
+        row = next(reader)
+        for k in "# portfolio key", "ncloc", "portfolio name", "last analysis", "URL":
+            assert k in row
+        for line in reader:
+            assert util.is_url(line[4])
+            assert line[3] == "" or util.is_datetime(line[3])
+            assert util.is_integer(line[1])
     util.clean(util.CSV_FILE)
 
 
@@ -164,17 +166,18 @@ def test_loc_proj_all_options_json() -> None:
     cmd = " ".join(JSON_OPTS + ALL_OPTIONS)
     if util.SQ.edition() == c.CE:
         util.run_failed_cmd(loc.main, cmd, errcodes.UNSUPPORTED_OPERATION)
-    else:
-        util.run_success_cmd(loc.main, cmd, post_cleanup=False)
-        # Check file contents
-        with open(file=util.JSON_FILE, mode="r", encoding="utf-8") as fh:
-            jsondata = json.loads(fh.read())
-        for component in jsondata:
-            for key in "branch", "lastAnalysis", "ncloc", "project", "projectName", "url":
-                assert key in component
-            assert component["ncloc"] == "" or util.is_integer(component["ncloc"])
-            assert util.is_url(component["url"])
-            assert component["lastAnalysis"] == "" or util.is_datetime(component["lastAnalysis"])
+        return
+
+    util.run_success_cmd(loc.main, cmd, post_cleanup=False)
+    # Check file contents
+    with open(file=util.JSON_FILE, mode="r", encoding="utf-8") as fh:
+        jsondata = json.loads(fh.read())
+    for component in jsondata:
+        for key in "branch", "lastAnalysis", "ncloc", "project", "projectName", "url":
+            assert key in component
+        assert component["ncloc"] == "" or util.is_integer(component["ncloc"])
+        assert util.is_url(component["url"])
+        assert component["lastAnalysis"] == "" or util.is_datetime(component["lastAnalysis"])
     util.clean(util.JSON_FILE)
 
 
@@ -183,17 +186,18 @@ def test_loc_apps_all_options_json() -> None:
     cmd = " ".join(JSON_OPTS + ALL_OPTIONS + ["--apps"])
     if util.SQ.edition() == c.CE:
         util.run_failed_cmd(loc.main, cmd, errcodes.UNSUPPORTED_OPERATION)
-    else:
-        util.run_success_cmd(loc.main, cmd, post_cleanup=False)
-        # Check file contents
-        with open(file=util.JSON_FILE, mode="r", encoding="utf-8") as fh:
-            jsondata = json.loads(fh.read())
-        for component in jsondata:
-            for key in "branch", "lastAnalysis", "ncloc", "app", "appName", "url":
-                assert key in component
-            assert component["ncloc"] == "" or util.is_integer(component["ncloc"])
-            assert util.is_url(component["url"])
-            assert component["lastAnalysis"] == "" or util.is_datetime(component["lastAnalysis"])
+        return
+
+    util.run_success_cmd(loc.main, cmd, post_cleanup=False)
+    # Check file contents
+    with open(file=util.JSON_FILE, mode="r", encoding="utf-8") as fh:
+        jsondata = json.loads(fh.read())
+    for component in jsondata:
+        for key in "branch", "lastAnalysis", "ncloc", "app", "appName", "url":
+            assert key in component
+        assert component["ncloc"] == "" or util.is_integer(component["ncloc"])
+        assert util.is_url(component["url"])
+        assert component["lastAnalysis"] == "" or util.is_datetime(component["lastAnalysis"])
     util.clean(util.JSON_FILE)
 
 
@@ -202,15 +206,16 @@ def test_loc_portfolios_all_options_json() -> None:
     cmd = " ".join(JSON_OPTS + ALL_OPTIONS + ["--portfolios"])
     if util.SQ.edition() in (c.CE, c.DE):
         util.run_failed_cmd(loc.main, cmd, errcodes.UNSUPPORTED_OPERATION)
-    else:
-        util.run_success_cmd(loc.main, cmd, post_cleanup=False)
-        # Check file contents
-        with open(file=util.JSON_FILE, mode="r", encoding="utf-8") as fh:
-            jsondata = json.loads(fh.read())
-        for component in jsondata:
-            for key in "lastAnalysis", "ncloc", "portfolio", "portfolioName", "url":
-                assert key in component
-            assert component["ncloc"] == "" or util.is_integer(component["ncloc"])
-            assert util.is_url(component["url"])
-            assert component["lastAnalysis"] == "" or util.is_datetime(component["lastAnalysis"])
+        return
+
+    util.run_success_cmd(loc.main, cmd, post_cleanup=False)
+    # Check file contents
+    with open(file=util.JSON_FILE, mode="r", encoding="utf-8") as fh:
+        jsondata = json.loads(fh.read())
+    for component in jsondata:
+        for key in "lastAnalysis", "ncloc", "portfolio", "portfolioName", "url":
+            assert key in component
+        assert component["ncloc"] == "" or util.is_integer(component["ncloc"])
+        assert util.is_url(component["url"])
+        assert component["lastAnalysis"] == "" or util.is_datetime(component["lastAnalysis"])
     util.clean(util.JSON_FILE)
