9fc2334c2f7b619c2dffa3c6377c35b99274c0fe
Olivier Korach <olivier.korach@sonarsource.com>
2025-06-28 09:27:29 +0200

    Adapt for older versions

--- test/unit/test_measures.py
+++ test/unit/test_measures.py
@@ -47,10 +47,12 @@ def test_measures_export(csv_file: Generator[str]) -> None:
     assert util.csv_col_float_pct(csv_file, "duplicated_lines_density", True)
     assert util.csv_col_float_pct(csv_file, "sqale_debt_ratio", True)
     assert not util.csv_cols_present(csv_file, "statements", "ncloc_language_distribution")
-    assert util.csv_cols_present(csv_file, "accepted_issues", "false_positive_issues", "prioritized_rule_issues")
-    assert util.csv_col_int(csv_file, "accepted_issues")
-    assert util.csv_col_int(csv_file, "false_positive_issues")
-    assert util.csv_col_int(csv_file, "prioritized_rule_issues")
+    col_list = ("false_positive_issues",)
+    if util.SQ.version() >= c.MQR_INTRO_VERSION:
+        col_list += ("accepted_issues", "prioritized_rule_issues")
+    assert util.csv_cols_present(csv_file, *col_list)
+    for col in col_list:
+        assert util.csv_col_int(csv_file, col)
 
 
 def test_measures_conversion(csv_file: Generator[str]) -> None:
@@ -85,10 +87,12 @@ def test_measures_export_json(json_file: Generator[str]) -> None:
     assert util.json_field_float(json_file, "duplicated_lines_density")
     assert util.json_field_float(json_file, "sqale_debt_ratio")
     assert util.json_fields_absent(json_file, "statements", "ncloc_language_distribution")
-    assert util.json_fields_present(json_file, "accepted_issues", "false_positive_issues", "prioritized_rule_issues")
-    assert util.json_field_int(json_file, "accepted_issues")
-    assert util.json_field_int(json_file, "false_positive_issues")
-    assert util.json_field_int(json_file, "prioritized_rule_issues")
+    col_list = col_list = ("false_positive_issues",)
+    if util.SQ.version() >= c.MQR_INTRO_VERSION:
+        col_list += ("accepted_issues", "prioritized_rule_issues")
+    assert util.json_fields_present(json_file, *col_list)
+    for col in col_list:
+        assert util.json_field_int(json_file, col)
 
 
 def test_measures_export_all(csv_file: Generator[str]) -> None:
