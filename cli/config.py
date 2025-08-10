8b3fcfbb2a082a4695a6a4709e40a55c5e60814a
Olivier Korach <olivier.korach@sonarsource.com>
2025-08-10 17:43:00 +0200

    Centralize SonarException handling

--- cli/config.py
+++ cli/config.py
@@ -267,26 +267,24 @@ def main() -> None:
         endpoint = platform.Platform(**kwargs)
         endpoint.verify_connection()
         endpoint.set_user_agent(f"{TOOL_NAME} {version.PACKAGE_VERSION}")
-    except (options.ArgumentsError, exceptions.ObjectNotFound) as e:
-        utilities.exit_fatal(e.message, e.errcode)
-    if not kwargs[options.EXPORT] and not kwargs[options.IMPORT]:
-        utilities.exit_fatal(f"One of --{options.EXPORT} or --{options.IMPORT} option must be chosen", exit_code=errcodes.ARGS_ERROR)
 
-    what = utilities.check_what(kwargs.pop(options.WHAT, None), WHAT_EVERYTHING, "exported or imported")
-    if options.WHAT_PROFILES in what and options.WHAT_RULES not in what:
-        what.append(options.WHAT_RULES)
-    kwargs[options.FORMAT] = utilities.deduct_format(kwargs[options.FORMAT], kwargs[options.REPORT_FILE], allowed_formats=("json", "yaml"))
-    if kwargs[options.EXPORT]:
-        try:
+        if not kwargs[options.EXPORT] and not kwargs[options.IMPORT]:
+            raise exceptions.SonarException(f"One of --{options.EXPORT} or --{options.IMPORT} option must be chosen", errcodes.ARGS_ERROR)
+
+        what = utilities.check_what(kwargs.pop(options.WHAT, None), WHAT_EVERYTHING, "exported or imported")
+        if options.WHAT_PROFILES in what and options.WHAT_RULES not in what:
+            what.append(options.WHAT_RULES)
+        kwargs[options.FORMAT] = utilities.deduct_format(kwargs[options.FORMAT], kwargs[options.REPORT_FILE], allowed_formats=("json", "yaml"))
+        if kwargs[options.EXPORT]:
             export_config(endpoint, what, **kwargs)
-        except exceptions.SonarException as e:
-            utilities.exit_fatal(e.message, e.errcode)
-        except (PermissionError, FileNotFoundError) as e:
-            utilities.exit_fatal(f"OS error while exporting config: {e}", exit_code=errcodes.OS_ERROR)
-    if kwargs["import"]:
-        if kwargs["file"] is None:
-            utilities.exit_fatal("--file is mandatory to import configuration", errcodes.ARGS_ERROR)
-        __import_config(endpoint, what, **kwargs)
+        elif kwargs[options.IMPORT]:
+            if kwargs["file"] is None:
+                utilities.exit_fatal("--file is mandatory to import configuration", errcodes.ARGS_ERROR)
+            __import_config(endpoint, what, **kwargs)
+    except (exceptions.SonarException, options.ArgumentsError) as e:
+        utilities.exit_fatal(e.message, e.errcode)
+    except (PermissionError, FileNotFoundError) as e:
+        utilities.exit_fatal(f"OS error while exporting config: {e}", exit_code=errcodes.OS_ERROR)
     utilities.stop_clock(start_time)
     cache_helper.clear_cache()
     sys.exit(0)
