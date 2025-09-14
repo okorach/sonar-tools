10188f2e66fcf917cf1dc109b5ea9f8ca8a1a22f
Olivier Korach <olivier.korach@sonarsource.com>
2025-09-14 19:01:42 +0200

    Fixes #1952 #1951

--- cli/config.py
+++ cli/config.py
@@ -114,9 +114,10 @@ def __write_export(config: dict[str, str], file: str, format: str) -> None:
 def __convert_for_yaml(json_export: dict[str, any]) -> dict[str, any]:
     """Converts the default JSON produced by export to a modified version more suitable for YAML"""
     for what in WHAT_EVERYTHING:
-        if what in json_export:
-            yamlify_func = _EXPORT_CALLS[what][2]
-            json_export[what] = yamlify_func(json_export[what])
+        for k in json_export:
+            if what.lower() == k.lower():
+                yamlify_func = _EXPORT_CALLS[what][2]
+                json_export[k] = yamlify_func(json_export[k])
     return json_export
 
 
