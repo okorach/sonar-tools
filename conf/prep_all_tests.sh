919692271b3d336dc54f614526d5945e86d52dd6
Olivier Korach <olivier.korach@sonarsource.com>
2025-10-03 18:09:21 +0200

    Shell quality pass

--- conf/prep_all_tests.sh
+++ conf/prep_all_tests.sh
@@ -29,31 +29,31 @@ function create_fresh_project {
     shift 3
     opts=("$@")
     opt_org=""
-    if [ "$url" == "https://sonarcloud.io" ]; then
+    if [ "${url}" == "https://sonarcloud.io" ]; then
         opt_org="-Dsonar.organization=okorach"
     fi
-    opt_token="-Dsonar.token=$token"
-    if [ "$url" == "$SONAR_HOST_URL_9" ]; then
-        opt_token="-Dsonar.login=$token"  
+    opt_token="-Dsonar.token=${token}"
+    if [ "${url}" == "${SONAR_HOST_URL_9}" ]; then
+        opt_token="-Dsonar.login=${token}"  
     fi
-    curl -X POST -u "$usertoken:" "$url/api/projects/delete?project=$key"
-    conf/scan.sh "${opts[@]}" -Dsonar.projectKey="$key" -Dsonar.projectName="$key" -Dsonar.host.url="$url" "$opt_token" "$opt_org" 
+    curl -X POST -u "${usertoken}:" "${url}/api/projects/delete?project=${key}"
+    conf/scan.sh "${opts[@]}" -Dsonar.projectKey="${key}" -Dsonar.projectName="${key}" -Dsonar.host.url="${url}" "${opt_token}" "${opt_org}" 
 }
 
-create_fresh_project "$SYNC_PROJECT_KEY" "${SONAR_HOST_URL_TEST:?}" "$SONAR_TOKEN_TEST_ADMIN_USER" "$SONAR_TOKEN_TEST_ADMIN_ANALYSIS" -nolint
-create_fresh_project "$SYNC_PROJECT_KEY" "${SONAR_HOST_URL_LATEST:?}" "$SONAR_TOKEN_LATEST_ADMIN_USER" "$SONAR_TOKEN_LATEST_ADMIN_ANALYSIS"
-create_fresh_project "$SYNC_PROJECT_KEY" "${SONAR_HOST_URL_CB:?}" "$SONAR_TOKEN_CB_ADMIN_USER" "$SONAR_TOKEN_CB_ADMIN_ANALYSIS"
-create_fresh_project "$SYNC_PROJECT_KEY" "${SONAR_HOST_URL_9:?}" "$SONAR_TOKEN_9_ADMIN_USER" "$SONAR_TOKEN_9_ADMIN_ANALYSIS"
-create_fresh_project "$SYNC_PROJECT_KEY" "https://sonarcloud.io" "$SONAR_TOKEN_SONARCLOUD" "$SONAR_TOKEN_SONARCLOUD"
-create_fresh_project "$SYNC_PROJECT_KEY" "${SONAR_HOST_URL_CB:?}" "$SONAR_TOKEN_CB_ADMIN_USER" "$SONAR_TOKEN_CB_ADMIN_ANALYSIS"
+create_fresh_project "${SYNC_PROJECT_KEY}" "${SONAR_HOST_URL_TEST:?}" "${SONAR_TOKEN_TEST_ADMIN_USER}" "${SONAR_TOKEN_TEST_ADMIN_ANALYSIS}" -nolint
+create_fresh_project "${SYNC_PROJECT_KEY}" "${SONAR_HOST_URL_LATEST:?}" "${SONAR_TOKEN_LATEST_ADMIN_USER}" "${SONAR_TOKEN_LATEST_ADMIN_ANALYSIS}"
+create_fresh_project "${SYNC_PROJECT_KEY}" "${SONAR_HOST_URL_CB:?}" "${SONAR_TOKEN_CB_ADMIN_USER}" "${SONAR_TOKEN_CB_ADMIN_ANALYSIS}"
+create_fresh_project "${SYNC_PROJECT_KEY}" "${SONAR_HOST_URL_9:?}" "${SONAR_TOKEN_9_ADMIN_USER}" "${SONAR_TOKEN_9_ADMIN_ANALYSIS}"
+create_fresh_project "${SYNC_PROJECT_KEY}" "https://sonarcloud.io" "${SONAR_TOKEN_SONARCLOUD" "${SONAR_TOKEN_SONARCLOUD"
+create_fresh_project "${SYNC_PROJECT_KEY}" "${SONAR_HOST_URL_CB:?}" "${SONAR_TOKEN_CB_ADMIN_USER}" "${SONAR_TOKEN_CB_ADMIN_ANALYSIS}"
 
 for pr in 5 7; do
-    sonar-scanner -Dsonar.host.url="${SONAR_HOST_URL_LATEST:?}" -Dsonar.pullrequest.key="$pr" -Dsonar.pullrequest.branch="feature/$pr" -Dsonar.token="$SONAR_TOKEN_LATEST_ADMIN_ANALYSIS"
-    sonar-scanner -Dsonar.host.url="${SONAR_HOST_URL_LTS:?}" -Dsonar.pullrequest.key="$pr" -Dsonar.pullrequest.branch="feature/$pr" -Dsonar.token="$SONAR_TOKEN_LTS_ADMIN_ANALYSIS"
+    sonar-scanner -Dsonar.host.url="${SONAR_HOST_URL_LATEST:?}" -Dsonar.pullrequest.key="${pr}" -Dsonar.pullrequest.branch="feature/${pr}" -Dsonar.token="${SONAR_TOKEN_LATEST_ADMIN_ANALYSIS}"
+    sonar-scanner -Dsonar.host.url="${SONAR_HOST_URL_LTS:?}" -Dsonar.pullrequest.key="${pr}" -Dsonar.pullrequest.branch="feature/${pr}" -Dsonar.token="${SONAR_TOKEN_LTS_ADMIN_ANALYSIS}"
 done
 # Format for 10.x and 9.x is different, file was generated for 10.x, so removing for 9.9
 
 rm build/external-issues*
 for pr in 5 7; do
-    sonar-scanner -Dsonar.host.url="${SONAR_HOST_URL_9:?}" -Dsonar.pullrequest.key="$pr" -Dsonar.pullrequest.branch="feature/$pr" -Dsonar.login="$SONAR_TOKEN_9_ADMIN_ANALYSIS"
+    sonar-scanner -Dsonar.host.url="${SONAR_HOST_URL_9:?}" -Dsonar.pullrequest.key="${pr}" -Dsonar.pullrequest.branch="feature/${pr}" -Dsonar.login="${SONAR_TOKEN_9_ADMIN_ANALYSIS}"
 done
