5427e03b1211f790bb3df0ba1191444dddc3f38a
Olivier Korach <olivier.korach@gmail.com>
2025-10-30 17:00:54 -0500

    Fix/2045 (#2048)
    * Fixes #2045
    
    * Fix version
    
    * Fix health check command
    
    * Formatting

--- sonar/util/project_utils.py
+++ sonar/util/project_utils.py
@@ -18,7 +18,7 @@
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 
-""" Project utility functions """
+"""Project utility functions"""
 
 import math
 from sonar import logging as log
