951be41f1f008da00a5deedec7732c201a689a8e
Olivier Korach <olivier.korach@sonarsource.com>
2025-11-05 16:38:53 +0100

    Add portfolios JSON conversion to new format

--- sonar/portfolios.py
+++ sonar/portfolios.py
@@ -850,3 +850,22 @@ def get_api_branch(branch: str) -> str:
 def clear_cache(endpoint: pf.Platform) -> None:
     """Clears the cache of an endpoint"""
     Portfolio.clear_cache(endpoint)
+
+
+def old_to_new_json_one(old_json: dict[str, Any]) -> dict[str, Any]:
+    new_json = old_json.copy()
+    for key in "children", "portfolios":
+        if key in new_json:
+            new_json[key] = old_to_new_json(new_json[key])
+    if "permissions" in old_json:
+        new_json["permissions"] = util.perms_to_list(old_json["permissions"])
+    if "branches" in old_json:
+        new_json["branches"] = util.dict_to_list(old_json["branches"], "name")
+    return new_json
+
+
+def old_to_new_json(old_json: dict[str, Any]) -> dict[str, Any]:
+    new_json = old_json.copy()
+    for k, v in new_json.items():
+        new_json[k] = old_to_new_json_one(v)
+    return util.dict_to_list(new_json, "key")
