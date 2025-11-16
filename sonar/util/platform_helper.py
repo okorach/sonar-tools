13463016491a1af46d843a151b4fb6e0e6b0bcc0
Olivier Korach <olivier.korach@sonarsource.com>
2025-11-16 14:39:50 +0100

    Adjust to JSON schema where webhooks is a list not a dict

--- sonar/util/platform_helper.py
+++ sonar/util/platform_helper.py
@@ -67,6 +67,7 @@ def convert_global_settings_json(old_json: dict[str, Any], full: bool = False) -
     for k in new_json["permissionTemplates"]:
         new_json["permissionTemplates"][k] = convert_template_json(new_json["permissionTemplates"][k], full)
     new_json["permissionTemplates"] = util.dict_to_list(new_json["permissionTemplates"], "key")
+    new_json["webhooks"] = util.dict_to_list(new_json["webhooks"], "name")
     new_json = common_json_helper.convert_common_fields(new_json)
 
     return util.order_dict(new_json, [*settings.CATEGORIES, "permissions", "permissionTemplates"])
