bb49e1afb6b8287401ad7957abc36fb2b5e75de9
Olivier Korach <olivier.korach@sonarsource.com>
2025-12-31 15:53:57 +0100

    Add tests

---
+++ test/unit/test_api_mgr.py
@@ -0,0 +1,36 @@
+#
+# sonar-tools tests
+# Copyright (C) 2025 Olivier Korach
+# mailto:olivier.korach AT gmail DOT com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+from sonar.api import manager as api_mgr
+import sonar.util.constants as c
+import utilities as tutil
+
+
+def test_api_mgr() -> None:
+    api_def = api_mgr.get_api_def("Group", c.REMOVE_MEMBER, tutil.SQ.version())
+    api, method, params = api_mgr.prep_params(api_def, id="membership_id", login="user.login", name="group.name")
+    if tutil.SQ.version() >= c.GROUP_API_V2_INTRO_VERSION:
+        assert api == "v2/authorizations/group-memberships/membership_id"
+        assert method == "DELETE"
+        assert params == {}
+    else:
+        assert api == "api/user_groups/remove_user"
+        assert method == "POST"
+        assert params == {"login": "user.login", "name": "group.name"}
