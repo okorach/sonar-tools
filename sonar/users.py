dab35f3a99845802611fefe190cb1729979e3a6c
Olivier Korach <olivier.korach@sonarsource.com>
2026-01-02 22:01:36 +0100

    Quality pass

--- sonar/users.py
+++ sonar/users.py
@@ -151,7 +151,7 @@ class User(SqObject):
         if endpoint.version() < c.USER_API_V2_INTRO_VERSION:
             raise exceptions.UnsupportedOperation("Get by ID is an APIv2 features, staring from SonarQube 10.4")
         log.debug("Getting user id '%s'", id)
-        api, _, params, ret = Api(cls, op.READ, endpoint).get_all(id=id)
+        api, _, params, _ = Api(cls, op.READ, endpoint).get_all(id=id)
         data = json.loads(endpoint.get(api, params=params, mute=()).text)
         return cls.load(endpoint, data)
 
