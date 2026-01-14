b22be87bb2080fc46131ce59e1e0a6985b983542
Olivier Korach <olivier.korach@sonarsource.com>
2026-01-14 23:04:36 +0100

    Don't reload non leaf object in init

--- sonar/components.py
+++ sonar/components.py
@@ -65,7 +65,6 @@ class Component(SqObject):
         self._last_analysis: Optional[datetime] = None
         self._visibility: Optional[str] = None
         self._new_code_start_date: Optional[datetime] = None
-        self.reload(data)
 
     def __str__(self) -> str:
         """String representation of object"""
