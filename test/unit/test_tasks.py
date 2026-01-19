a53e58b21ce43e76cf9885e912a690252317ea8b
Olivier Korach <olivier.korach@sonarsource.com>
2026-01-19 16:18:27 +0100

    Formatting

--- test/unit/test_tasks.py
+++ test/unit/test_tasks.py
@@ -70,6 +70,7 @@ def test_no_scanner_context() -> None:
     settings = {}
     task.audit(settings)
 
+
 # Test does not work - You can't request branch master when scan happened without the branch spec
 # def test_search_branch() -> None:
 #     """test_search_branch"""
