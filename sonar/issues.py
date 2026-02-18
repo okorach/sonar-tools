a572e00958072a2a74781c722e2a06af65f1db38
Olivier K <161828284+okorach-sonar@users.noreply.github.com>
2026-02-18 14:34:40 +0100

    Fix-2274 (#2275)
    * Fixes #2274
    
    * Reduce complexity

--- sonar/issues.py
+++ sonar/issues.py
@@ -48,6 +48,8 @@ if TYPE_CHECKING:
 
 
 _MAX_FACETS = 100
+
+_MAX_FACETS = 100
 _OLD_SEARCH_COMPONENT_FIELD = "componentKeys"
 _NEW_SEARCH_COMPONENT_FIELD = "components"
 
@@ -196,14 +198,15 @@ class Issue(findings.Finding):
         new_params = cls.sanitize_search_params(endpoint, **search_params) | {"project": project}
         if endpoint.edition() in (c.EE, c.DCE) and search_findings:
             log.debug("Using export_findings() to speed up issue export")
-            issue_list: dict[str, Issue] = findings.export_findings(endpoint, project, new_params.get("branch"), new_params.get("pullRequest"))
-        else:
+            return findings.export_findings(endpoint, project, new_params.get("branch"), new_params.get("pullRequest"))
+
+        try:
+            issue_list = cls.search_unsafe(endpoint, **new_params)
+        except TooManyIssuesError as e:
+            log.info("%s - Recursing and slicing the search by date", e.message)
+            date_start = get_oldest_issue(endpoint, params=new_params)
+            date_stop = get_newest_issue(endpoint, params=new_params)
             try:
-                issue_list = cls.search_unsafe(endpoint, **new_params)
-            except TooManyIssuesError as e:
-                log.info("%s - Recursing and slicing the search by date", e.message)
-                date_start = get_oldest_issue(endpoint, params=new_params)
-                date_stop = get_newest_issue(endpoint, params=new_params)
                 issue_list = cls.search_by_date(endpoint, date_start=date_start, date_stop=date_stop, **new_params)
             except TooManyIssuesError as e:
                 # In last resort, use export_findings() to avoid exceeding the 10K limit
@@ -263,7 +266,7 @@ class Issue(findings.Finding):
         try:
             issue_list = cls.search_unsafe(endpoint, **new_params)
         except TooManyIssuesError as e:
-            log.info("%s - Recursing and slicing the search by file", "%s - Recursing and slicing the search by file", e.message)
+            log.info("%s - Recursing and slicing the search by file", e.message)
             facets = _get_facets(endpoint, project, facet="files", **new_params)
             if len(facets) == _MAX_FACETS:
                 raise TooManyIssuesError(len(facets), f"Too many files (>={len(facets)}) in facets")
