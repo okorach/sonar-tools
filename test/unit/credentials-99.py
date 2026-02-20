ac719c4a07eb76f6f8cf95dd2f721105ad29d574
Olivier Korach <olivier.korach@sonarsource.com>
2026-02-20 15:42:01 +0100

    Fix 9.9 credentials

--- test/unit/credentials-99.py
+++ test/unit/credentials-99.py
@@ -23,7 +23,7 @@ from os import getenv
 from datetime import datetime
 
 TARGET_PLATFORM = "http://localhost:9900"
-TARGET_TOKEN = getenv("SONAR_TOKEN_9_ADMIN_USER")
+TARGET_TOKEN = getenv("SONAR_TOKEN_99_ADMIN_USER")
 ORGANIZATION = None
 
 ISSUE_FP = "AZrWSZgXG2ojjjMGNBfu"
