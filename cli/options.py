6dd206286cd8cc9b93e7b0827909abde963f9638
Olivier Korach <olivier.korach@gmail.com>
2026-04-02 09:30:38 +0200

    Fix --validate - should ignore any other params

--- cli/options.py
+++ cli/options.py
@@ -213,7 +213,11 @@ def parse_and_check(parser: ArgumentParser, logger_name: Optional[str] = None, v
         kwargs[URL] = kwargs[URL].replace("http://localhost", "http://host.docker.internal")
     kwargs = __convert_args_to_lists(kwargs=kwargs)
     log.debug("CLI arguments = %s", util.json_dump(sutil.redact_tokens(kwargs)))
-    if not kwargs.get(IMPORT, False) and not kwargs.get(VALIDATE_FILE, False) and not kwargs.get(CONVERT_FROM, False):
+    if kwargs.get(VALIDATE_FILE, False) or kwargs.get(CONVERT_FROM, False):
+        if not kwargs.get(REPORT_FILE, False):
+            raise ArgumentsError(f"File (--{REPORT_FILE} option) is mandatory with options --{VALIDATE_FILE} or --{CONVERT_FROM}")
+        return args
+    if not kwargs.get(IMPORT, False):
         __check_file_writeable(kwargs.get(REPORT_FILE))
     # Verify version randomly once every 10 runs
     if not kwargs[SKIP_VERSION_CHECK] and random.randrange(10) == 0:
